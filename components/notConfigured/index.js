5df49a822ae232f5feb15bda27665aedc47ed3cb
Max Eddy <maxseddy@gmail.com>
2018-06-19 12:37:49 -0700

    Recursively render api response.

--- components/notConfigured/index.js
+++ components/notConfigured/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const NotConfigured = (props) => (
-  <h1>Component {`"${props.name}"`} is not configured.</h1>
+  <React.Fragment>
+    <h1>Component {`"${props.name}"`} is not configured.</h1>
+    {props.children}
+  </React.Fragment>
 );
 
 NotConfigured.propTypes = {
   name: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default NotConfigured;
