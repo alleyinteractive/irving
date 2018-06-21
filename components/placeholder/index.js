86f512cc635d321fbd7d043ced14164f13b26999
Max Eddy <max@alley.co>
2018-06-21 11:31:19 -0700

    Render placeholder children more clearly.

--- components/placeholder/index.js
+++ components/placeholder/index.js
@@ -5,17 +5,30 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 import styles from './placeholder.css';
 
-const Placeholder = (props) => (
-  <div className={styles.wrapper}>
-    <h1 className={styles.heading}>{props.name}</h1>
-    <pre>{JSON.stringify(omit(['name', 'children'], props), null, 2)}</pre>
-    {props.children}
-  </div>
-);
+const Placeholder = (props) => {
+  const { name, children } = props;
+  const level = 6 < props.level ? 6 : props.level;
+  const Heading = `h${level}`;
+  return (
+    <div className={styles.wrapper}>
+      <Heading className={styles[`heading${level}`]}>{name}</Heading>
+      <pre>{JSON.stringify(omit(['name', 'children'], props), null, 2)}</pre>
+      <div className={styles.children}>
+        {React.Children.map(children, (child) =>
+          React.cloneElement(child, { level: level + 1 }))}
+      </div>
+    </div>
+  );
+};
 
 Placeholder.propTypes = {
   name: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  level: PropTypes.number,
+};
+
+Placeholder.defaultProps = {
+  level: 2,
 };
 
 const wrapWithStyles = withStyles(styles);
