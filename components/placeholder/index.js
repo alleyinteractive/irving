b474132b71d730390f833e3cae954c4697990db1
Max Eddy <max@alley.co>
2018-06-21 11:40:08 -0700

    If a component has a url prop then add an anchor tag.

--- components/placeholder/index.js
+++ components/placeholder/index.js
@@ -9,9 +9,13 @@ const Placeholder = (props) => {
   const { name, children } = props;
   const level = 6 < props.level ? 6 : props.level;
   const Heading = `h${level}`;
+  const headingElement = (
+    <Heading className={styles[`heading${level}`]}>{name}</Heading>
+  );
   return (
     <div className={styles.wrapper}>
-      <Heading className={styles[`heading${level}`]}>{name}</Heading>
+      {!! props.url && <a href={props.url}>{headingElement}</a>}
+      {! props.url && headingElement}
       <pre>{JSON.stringify(omit(['name', 'children'], props), null, 2)}</pre>
       <div className={styles.children}>
         {React.Children.map(children, (child) =>
@@ -25,10 +29,12 @@ Placeholder.propTypes = {
   name: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
   level: PropTypes.number,
+  url: PropTypes.string,
 };
 
 Placeholder.defaultProps = {
   level: 2,
+  url: '',
 };
 
 const wrapWithStyles = withStyles(styles);
