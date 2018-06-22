b43fca55d3f3f9c543b58dcd1d839927317c43a0
Max Eddy <max@alley.co>
2018-06-22 12:08:09 -0700

    url test.

--- utils/isNode.js
+++ utils/isNode.js
@@ -1,5 +1,9 @@
-const isNode = () => 'undefined' !== typeof process &&
-  process.release &&
-  'node' === process.release.name;
-
-export default isNode;
+/**
+ * Reasonable detect if we are executing in a NodeJS environment.
+ * @returns {boolean}
+ */
+export default function isNode() {
+  return 'undefined' !== typeof process &&
+    process.release &&
+    'node' === process.release.name;
+}
