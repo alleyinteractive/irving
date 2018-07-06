2a43884a36d9ec94de218d20fb6b5054c603a660
Max Eddy <maxseddy@gmail.com>
2018-07-06 08:40:48 -0700

    Backport basic auth.

--- server/production.js
+++ server/production.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const auth = require('./auth');
 // App must be built using the build command before production mode can be run.
 const clientStats = require('../build/client/stats.json');
 const { default: serverRenderer } = require('../build/server/main.bundle');
@@ -9,6 +10,8 @@ const { default: serverRenderer } = require('../build/server/main.bundle');
  * @param {object} app - express application
  */
 const productionMiddleware = (app) => {
+  app.use(auth);
+
   app.use(express.static(path.resolve('./build/client'), {
     maxage: 86400000,
   }));
