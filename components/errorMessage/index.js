a8d21ef1be53a4dda7ffd1cd95c8f8824d550e86
Max Eddy <max@alley.co>
2018-07-10 15:13:31 -0700

    Basic error page styles.

--- components/errorMessage/index.js
+++ components/errorMessage/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+import styles from './errorMessage.css';
 
 const ErrorMessage = () => (
-  <React.Fragment>
-    <h1>Something has gone wrong.</h1>
+  <div className={styles.wrapper}>
+    <h1 className={styles.heading}>Something has gone wrong.</h1>
     <p>You can try refreshing the page. The problem may be temporary.</p>
     <p>We have been notified of the problem, and are working to resolve it.</p>
-  </React.Fragment>
+  </div>
 );
 
-export default ErrorMessage;
+const wrapWithStyles = withStyles(styles);
+
+export default wrapWithStyles(ErrorMessage);
