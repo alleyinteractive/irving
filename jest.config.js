e3e76863abd2ff5aa986062db915a2d4e173974c
Max Eddy <max@alley.co>
2018-07-20 12:54:11 -0700

    Configure jest.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   testURL: 'http://localhost',
+  transform: {
+    '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/utils/mockCssTransform.js',
+  },
 };
