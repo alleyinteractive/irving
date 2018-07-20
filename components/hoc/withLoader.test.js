dc5b63bf936717855fc28a443697735b29f656f5
Max Eddy <maxseddy@gmail.com>
2018-07-20 12:54:11 -0700

    Configure jest.

--- components/hoc/withLoader.test.js
+++ components/hoc/withLoader.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import PlaceholderLoading from 'components/placeholderLoading';
 import withLoader from './withLoader';
+
+configure({ adapter: new Adapter() });
 
 it('should skip rendering wrapped component if loading', () => {
   const Foo = () => <div>foo</div>;
