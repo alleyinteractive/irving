489ba3598edc24eb7a604dd3b2916fe0f777ec8d
Owen Stowe <ostowe@gmail.com>
2018-09-28 12:57:45 -0400

    Test out wrapper component for providing redux state
    test out styles also

--- components/form/inputRadio.md
+++ components/form/inputRadio.md
@@ -2,7 +2,7 @@ Radio input example:
 
 ```js
 <InputRadio 
-    id={0} 
+    id="testRadio" 
     name="testRadio" 
     label="This is a sample radio button component" 
     onChange={() => {}} 
---
+++ styleguide/wrapper.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { StyleContext, CriticalCssBuilder } from 'critical-style-loader/lib';
+import rootReducer from 'reducers';
+import defaultState from 'reducers/defaultState';
+
+const store = createStore(
+  rootReducer,
+  {
+    ...defaultState,
+    components: {
+      defaults: [],
+      page: {
+        '/': [
+          {
+            name: 'body',
+            config: {
+              name: 'body',
+            },
+            children: [],
+          },
+        ],
+      },
+    },
+    route: {
+      pathname: '/',
+    },
+  }
+);
+// Container for critical css related to this page render.
+const cssBuilder = new CriticalCssBuilder();
+
+const Wrapper = (props) => {
+  const { children } = props;
+  return (
+    <Provider store={store}>
+      <StyleContext.Provider value={cssBuilder.addCss}>
+        {children}
+      </StyleContext.Provider>
+    </Provider>
+  );
+};
+
+Wrapper.propTypes = {
+  children: PropTypes.object.isRequired,
+};
+
+export default Wrapper;
