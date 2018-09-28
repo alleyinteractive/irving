422ef785c370826d24891749535e0a156964b058
Owen Stowe <ostowe@gmail.com>
2018-09-28 12:57:45 -0400

    Test out wrapper component for providing redux state
    test out styles also

--- config/styleguide.config.js
+++ config/styleguide.config.js
@@ -3,4 +3,8 @@ const path = require('path');
 module.exports = {
   components: path.resolve('components/**/*.js'),
   webpackConfig: require('./webpack.config.js')({}, { mode: 'development' })[0],
+  skipComponentsWithoutExample: true,
+  styleguideComponents: {
+    Wrapper: path.join(__dirname, '../styleguide/wrapper.js'),
+  },
 };
