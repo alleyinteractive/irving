7acd146c860057bc6cf6d15933dd9b677edcbd39
Owen Stowe <ostowe@gmail.com>
2018-09-28 15:11:20 -0400

    Test out styleguide build functionality

--- config/styleguide.config.js
+++ config/styleguide.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const paths = require('./paths');
 
 module.exports = {
-  components: path.resolve('components/**/*.js'),
+  components: path.join(paths.appRoot, 'components/**/*.js'),
   webpackConfig: require('./webpack.config.js')({}, { mode: 'development' })[0],
   skipComponentsWithoutExample: true,
   styleguideComponents: {
-    Wrapper: path.join(__dirname, '../styleguide/wrapper.js'),
+    Wrapper: path.join(paths.styleguideRoot, './components/wrapper.js'),
   },
+  styleguideDir: paths.styleguideRoot,
 };
