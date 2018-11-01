3c44f5f07bd009d75034946b9a8002affe9b08c6
Owen Stowe <ostowe@gmail.com>
2018-11-01 18:20:28 -0400

    Update forms to be more robust/complete
    - Combine radios and checkboxes into a single component
    - Allow labels to contain child elements

--- components/form/textArea.js
+++ components/form/textArea.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from 'critical-style-loader/lib';
+import RawHTML from 'components/rawHTML';
 import Label from './label';
 import styles from './text.css';
 
@@ -12,6 +13,7 @@ const TextArea = (props) => {
     required,
     className,
     error,
+    children,
   } = props;
   return (
     <div className={classNames(
@@ -22,13 +24,14 @@ const TextArea = (props) => {
     >
       <Label
         htmlFor={name}
-        text={label}
-        required={required}
+        require={required}
         className={styles.label}
-      />
+      >
+        {children}
+      </Label>
       <textarea {...props} className={styles.textarea} type="text" />
       {!! error &&
-        <span className={styles.errorText}>{error}</span>
+        <span className={styles.errorText}><RawHTML content={error} /></span>
       }
     </div>
   );
@@ -44,6 +47,12 @@ TextArea.propTypes = {
   className: PropTypes.string,
   rows: PropTypes.number,
   error: PropTypes.string,
+  children: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.string,
+    ])
+  ).isRequired,
 };
 
 TextArea.defaultProps = {
