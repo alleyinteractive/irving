82b63e32213dc3063357d642f9e978c7751a9794
Owen Stowe <ostowe@gmail.com>
2018-12-10 09:25:20 -0500

    Switch to loadScript utility so we can use async/await for reset

--- components/disqus/index.js
+++ components/disqus/index.js
@@ -1,67 +1,38 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import validateDisqusConfig from './validateDisqusConfig';
+import loadScript from 'utils/loadScript';
+import validateDisqusConfig from './validate';
 
 class DisqusEmbed extends Component {
-  constructor(props) {
-    super(props);
+  async componentDidMount() {
+    const { forumShortname } = this.props;
+    const config = validateDisqusConfig(this.props, window.location.pathname);
 
-    this.config = {};
-  }
-
-  state = {
-    ready: false,
-  };
-
-  componentDidMount() {
-    this.init();
-    this.config = validateDisqusConfig(this.props, window.location.pathname);
-  }
-
-  onLoad() {
-    if (this.config) {
-      // @see - https://help.disqus.com/customer/portal/articles/472107
-      // Reloading a Disqus thread within an AJAX application:
-      window.DISQUS.reset({
-        reload: true,
-        config() {
-          this.page.identifier = this.config.pageIdentifier;
-          this.page.url = this.config.pageUrl;
-        },
-      });
+    if (! config) {
+      return;
     }
-  }
 
-  init = () => {
-    this.setState({ ready: true });
-  };
+    // This script must load only when an anchor element exists in the DOM.
+    await loadScript(
+      `https://${forumShortname}.disqus.com/embed.js`,
+      'disqus-embed'
+    );
+
+    // @see - https://help.disqus.com/customer/portal/articles/472107
+    // Reloading a Disqus thread within an AJAX application:
+    window.DISQUS.reset({
+      reload: true,
+      config() {
+        this.page.identifier = config.pageIdentifier;
+        this.page.url = config.pageUrl;
+      },
+    });
+  }
 
   render() {
-    const {
-      pageUrl,
-      pageIdentifier,
-      forumShortname,
-    } = this.props;
-
-    if (! pageUrl || ! pageIdentifier || ! forumShortname) {
-      return null;
-    }
-
     return (
       <Fragment>
         <div id="disqus_thread" />
-        <script
-          dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-            __html: ! this.state.ready ? '' :
-              `(function() {
-                var d = document, s = d.createElement('script');
-                s.src = 'https://${forumShortname}.disqus.com/embed.js';
-                s.setAttribute('data-timestamp', +new Date());
-                (d.head || d.body).appendChild(s);
-              })();`,
-          }}
-          onLoad={this.onLoad}
-        />
         <noscript
           dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
             __html: 'Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>',
@@ -73,8 +44,10 @@ class DisqusEmbed extends Component {
 }
 
 DisqusEmbed.propTypes = {
+  /* eslint-disable react/no-unused-prop-types */
   pageUrl: PropTypes.string.isRequired,
   pageIdentifier: PropTypes.string.isRequired,
+  /* eslint-enable */
   forumShortname: PropTypes.string.isRequired,
 };
 
