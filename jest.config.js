7b20051f4490c221b649857b2d1a16463e049e87
Owen Stowe <ostowe@gmail.com>
2019-01-15 14:47:37 -0500

    More updates for jest

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,6 @@ module.exports = {
     '^.+\\.css$': '<rootDir>/utils/mockCssTransform.js',
   },
   testEnvironment: 'enzyme',
-  setupTestFrameworkScriptFile: 'jest-enzyme',
   setupFiles: ['<rootDir>/node_modules/regenerator-runtime/runtime'],
-  testEnvironmentOptions: {
-    enzymeAdapter: 'react16',
-  },
+  setupTestFrameworkScriptFile: 'jest-enzyme',
 };
