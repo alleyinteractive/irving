e046f7e1fb23cb450cf1b05fa0aba024e41d8763
Owen Stowe <ostowe@gmail.com>
2019-01-15 14:59:45 -0500

    Reinstate babel 7 as we're actually ok here

--- config/webpack/entry.js
+++ config/webpack/entry.js
@@ -7,7 +7,7 @@ const { serverRoot, clientRoot, proxyUrl } = require('../paths');
  * @returns {string|array|object} - a entry configuration value
  */
 module.exports = function getEntry(context) {
-  const polyfills = ['babel-polyfill', 'isomorphic-fetch'];
+  const polyfills = ['@babel/polyfill', 'isomorphic-fetch'];
   switch (context) {
     case 'production_server':
     case 'development_server':
