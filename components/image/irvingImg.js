9ee8892f409b2cf6b57eb01f9b463c99e15020b3
Owen Stowe <ostowe@gmail.com>
2019-02-06 11:04:44 -0500

    Fix missing Component import

--- components/image/irvingImg.js
+++ components/image/irvingImg.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'critical-style-loader/lib';
 import styles from './image.css';
