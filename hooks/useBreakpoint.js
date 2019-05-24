e89489b112c16830e5cbc93082b716c805d2fe5d
Owen Stowe <ostowe@gmail.com>
2019-05-24 15:28:19 -0400

    Add some docs, fix useLoadScript a bit.

--- hooks/useBreakpoint.js
+++ hooks/useBreakpoint.js
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react';
 import breakpoints from 'config/css/breakpoints';
 
+/**
+ * Hook for creating a script tag for an external script and inserting it into the DOM.
+ *
+ * @param {string} breakpointName - Breakpoint name. Must correspond to one of the keys in config/css/breakpoints.js
+ * @returns {bool} - Does the viewport width match this breakpoint/media query?
+ */
 export default function useBreakpoint(breakpointName) {
   if (breakpoints[breakpointName]) {
     const mq = window.matchMedia(`(${breakpoints[breakpointName]})`);
