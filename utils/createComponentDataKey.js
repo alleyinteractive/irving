7fc36909def5eee6c9bd91c0276e803c036331aa
Owen Stowe <ostowe@gmail.com>
2019-05-31 15:55:32 -0400

    Add another note about createComponentDataKey

--- utils/createComponentDataKey.js
+++ utils/createComponentDataKey.js
@@ -2,6 +2,8 @@ import { replace } from 'lodash/fp';
 
 /**
  * Create a unique key for use in global state for components with external data.
+ * This function is in part necessary to prevent calls to lodash/set from breaking due to
+ * `.` characters within the key.
  *
  * @returns {string} - Key
  */
