2a39d6c2acf54d71501041f871b066b998cc9bd9
Zack Rothauser <zack@alley.co>
2019-06-26 11:10:35 -0700

    Fixes links to relative paths if they include search queries

--- utils/getRelativeUrl.test.js
+++ utils/getRelativeUrl.test.js
@@ -4,8 +4,32 @@ it('should handle a relative url', () => {
   expect(getRelativeUrl('/foo')).toBe('/foo');
 });
 
+it('should handle a relative url with search parameters', () => {
+  expect(getRelativeUrl('/foo/?param=test&another=second'))
+    .toBe('/foo/?param=test&another=second');
+});
+
+it('should handle a relative url with a hash', () => {
+  expect(getRelativeUrl('/foo#somehash'))
+    .toBe('/foo#somehash');
+});
+
 it('should handle an internal absolute url', () => {
   expect(getRelativeUrl('http://localhost/foo')).toBe('/foo');
+});
+
+it('should handle an internal absolute url with search query', () => {
+  expect(getRelativeUrl('http://localhost/foo/?s=test')).toBe('/foo/?s=test');
+});
+
+it('should handle an internal absolute url with hash', () => {
+  expect(getRelativeUrl('http://localhost/foo#test'))
+    .toBe('/foo#test');
+});
+
+it('should handle an internal absolute url with search query and hash', () => {
+  expect(getRelativeUrl('http://localhost/foo/?s=search#test'))
+    .toBe('/foo/?s=search#test');
 });
 
 it('should handle an external absolute url', () => {
