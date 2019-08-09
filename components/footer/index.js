62642d7270f2e1404b4bbb34eb17a4d4b09a547c
Pattie Reaves <pattie@alley.co>
2019-08-09 14:45:44 -0700

    MIT-109: Basic footer styling.

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { __ } from '@wordpress/i18n';
 import { withStyles } from 'critical-style-loader/lib';
 import styles from './footer.css';
 
-const Footer = () => (
-  <footer className={styles.wrapper}>
-    This is your footer
+const Footer = ({ children, copyrightLink, missionStatement }) => (
+  <footer className={styles.container} aria-labelledby="footer">
+    <div className={styles.wrapper}>
+      <h2 id="footer" className={styles.title}>
+        {__('MIT Technology Review', 'mittr')}
+      </h2>
+      <div className={styles.missionStatement}>{missionStatement}</div>
+      <a href="/editions/" className={styles.editionsLink}>
+        {__('Browse international editions', 'mittr')}
+      </a>
+      {children}
+      <div className={styles.copyright}>
+        <a href={copyrightLink}>MIT Technology Review © 2019</a>
+      </div>
+    </div>
   </footer>
 );
+
+Footer.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  missionStatement: PropTypes.string.isRequired,
+  copyrightLink: PropTypes.string.isRequired,
+};
 
 export default withStyles(styles)(Footer);
--- config/css/layout.js
+++ config/css/layout.js
@@ -2,4 +2,5 @@ module.exports = {
   columns: 12,
   gutterGrid: `${20 / 16}rem`,
   gutterOutside: `${20 / 16}rem`,
+  siteMaxWidth: '77.5rem',
 };
