6c87ed639ae1ec042c87a46b763dc2d92504f425
Pattie Reaves <pattie@pzunk.net>
2019-08-14 17:27:24 -0700

    MIT-111: Update article text styles.

--- config/css/fonts.js
+++ config/css/fonts.js
@@ -4,6 +4,6 @@
 module.exports = {
   sans: '\'NeueHaas\', input mono, sans-serif',
   serif: '\'MillerText\', input mono, serif',
-  'serif-text': '\'MillerText\', input mono, serif',
+  'serif-text': 'Independent, serif',
   'serif-display': '\'MillerDisplay\', input mono, serif',
 };
