0f2306678a58ab10644ca456002b41fd6c0595e6
Owen Stowe <ostowe@gmail.com>
2019-08-18 15:03:56 -0400

    Gitignore all node modules, keept attempting to set up testing

---
+++ packages/forms/config/jest.config.js
@@ -0,0 +1,13 @@
+const baseConfig = require('../../../jest.config.base.js');
+
+module.exports = {
+  ...baseConfig,
+  name: 'irving-forms',
+  displayName: 'irving-forms',
+  setupFiles: ['<rootDir>/config/jest.setup.js'],
+  setupFilesAfterEnv: ['jest-enzyme'],
+  testEnvironment: 'enzyme',
+  testEnvironmentOptions: {
+    enzymeAdapter: 'react16',
+  },
+};
