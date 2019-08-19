179e9a11b61d71c311e7cd34786748fa7a9cedc6
Owen Stowe <ostowe@gmail.com>
2019-08-19 15:02:08 -0400

    Begin work on binaries, cli

--- packages/core/bin/irving.js
+++ packages/core/bin/irving.js
@@ -1,111 +1,44 @@
 #!/usr/bin/env node
-import arg from 'next/dist/compiled/arg/index.js'
-;['react', 'react-dom'].forEach(dependency => {
+/* eslint-disable global-require, max-len */
+['react', 'react-dom'].forEach((dependency) => {
   try {
     // When 'npm link' is used it checks the clone location. Not the project.
-    require.resolve(dependency)
+    require.resolve(dependency);
   } catch (err) {
     console.warn(
       `The module '${dependency}' was not found. Irving requires that you include it in 'dependencies' of your 'package.json'. To add it, run 'npm install --save ${dependency}'`
-    )
+    );
   }
-})
+});
 
-const defaultCommand = 'dev'
-export type cliCommand = (argv?: string[]) => void
-const commands: { [command: string]: () => Promise<cliCommand> } = {
-  build: async () => await import('../cli/next-build').then(i => i.nextBuild),
-  start: async () => await import('../cli/next-start').then(i => i.nextStart),
-  export: async () =>
-    await import('../cli/next-export').then(i => i.nextExport),
-  dev: async () => await import('../cli/next-dev').then(i => i.nextDev),
-}
-
-const args = arg(
-  {
-    // Types
-    '--version': Boolean,
-    '--help': Boolean,
-    '--inspect': Boolean,
-
-    // Aliases
-    '-v': '--version',
-    '-h': '--help',
+const defaultCommand = 'dev';
+const commandMap = {
+  build: () => {
+    process.env.NODE_ENV = 'production';
+    require('../cli/build');
   },
-  {
-    permissive: true,
+  start: () => {
+    process.env.NODE_ENV = 'production';
+    require('../cli/start');
+  },
+  dev: () => {
+    process.env.NODE_ENV = 'development';
+    require('../cli/dev');
   }
-)
+};
+const commands = Object.keys(commandMap);
 
-// Version is inlined into the file using taskr build pipeline
-if (args['--version']) {
-  // tslint:disable-next-line
-  console.log(`Next.js v${process.env.__NEXT_VERSION}`)
-  process.exit(0)
-}
-
-// Check if we are running `next <subcommand>` or `next`
-const foundCommand = Boolean(commands[args._[0]])
-
-// Makes sure the `next <subcommand> --help` case is covered
-// This help message is only showed for `next --help`
-if (!foundCommand && args['--help']) {
-  // tslint:disable-next-line
-  console.log(`
-    Usage
-      $ next <command>
-
-    Available commands
-      ${Object.keys(commands).join(', ')}
-
-    Options
-      --version, -v   Version number
-      --inspect       Enable the Node.js inspector
-      --help, -h      Displays this message
-
-    For more information run a command with the --help flag
-      $ next build --help
-  `)
-  process.exit(0)
-}
-
-const command = foundCommand ? args._[0] : defaultCommand
-const forwardedArgs = foundCommand ? args._.slice(1) : args._
-
-if (args['--inspect'])
-  throw new Error(
-    `Use env variable NODE_OPTIONS instead: NODE_OPTIONS="--inspect" next ${command}`
-  )
-
-// Make sure the `next <subcommand> --help` case is covered
-if (args['--help']) {
-  forwardedArgs.push('--help')
-}
-
-const defaultEnv = command === 'dev' ? 'development' : 'production'
-;(process.env as any).NODE_ENV = process.env.NODE_ENV || defaultEnv
-
-// this needs to come after we set the correct NODE_ENV or
-// else it might cause SSR to break
-const React = require('react')
-
-if (typeof React.Suspense === 'undefined') {
-  throw new Error(
-    `The version of React you are using is lower than the minimum required version needed for Next.js. Please upgrade "react" and "react-dom": "npm install --save react react-dom" https://err.sh/zeit/next.js/invalid-react-version`
-  )
-}
-
-commands[command]().then(exec => exec(forwardedArgs))
-
-if (command === 'dev') {
-  const { CONFIG_FILE } = require('next-server/constants')
-  const { watchFile } = require('fs')
-  watchFile(`${process.cwd()}/${CONFIG_FILE}`, (cur: any, prev: any) => {
-    if (cur.size > 0 || prev.size > 0) {
-      // tslint:disable-next-line
-      console.log(
-        `\n> Found a change in ${CONFIG_FILE}. Restart the server to see the changes in effect.`
-      )
+if (process.argv && Array.isArray(process.argv)) {
+  const command = process.argv.find((arg) => {
+    if (commands.find((command) => arg === command)) {
+      return true;
     }
-  })
+
+    return false;
+  });
+
+  if (commandMap[command]) {
+    commandMap[command]();
+  }
 }
+/* eslint-enable */
---
+++ packages/core/cli/build.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const getConfig = require('../config/webpack.config.js');
+const config = getConfig({}, { mode: 'development' });
+
+// Compile.
+webpack(
+  config,
+  (err, stats) => {
+    // Log fatal webpack errors.
+    if (err) {
+      console.error(err.stack || err);
+      if (err.details) {
+        console.error(err.details);
+      }
+      return;
+    }
+
+    const info = stats.toJson();
+
+    // Log compile errors.
+    if (stats.hasErrors()) {
+      console.error(info.errors);
+    }
+
+    // Log compile warnings.
+    if (stats.hasWarnings()) {
+      console.warn(info.warnings);
+    }
+
+    console.log('build complete');
+  }
+);
