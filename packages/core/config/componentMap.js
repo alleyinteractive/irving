e112629d4fcfb887f9118dca3e628d54bc715b0d
Owen Stowe <ostowe@gmail.com>
2019-08-19 16:59:06 -0400

    Remove unnecessary components from componentMap
    - Remove integrations into their own package

--- packages/core/config/componentMap.js
+++ packages/core/config/componentMap.js
@@ -1,17 +1,10 @@
 import AudioElement from 'components/audio';
 import Body from 'components/body';
-import Footer from 'components/footer';
-import Disqus from 'components/disqus';
-import GoogleTagManager from 'components/googleTagManager';
-import GoogleAnalytics from 'components/googleAnalytics';
 import Head from 'components/head';
 import Image from 'components/image/image';
 import NotConfigured from 'components/notConfigured';
 import RawHTML from 'components/rawHTML';
-import Parsely from 'components/parsely';
 import Placeholder from 'components/placeholder';
-import SocialList from 'components/socialList';
-import SocialItem from 'components/socialItem';
 import withLoader from 'components/hoc/withLoader';
 
 /**
@@ -21,21 +14,13 @@ export const componentMap = {
   'admin-bar': Placeholder,
   'audio-element': AudioElement,
   body: withLoader(Body),
-  disqus: Disqus,
   embed: RawHTML,
-  footer: Footer,
-  'google-analytics': GoogleAnalytics,
-  'google-tag-manager': GoogleTagManager,
   head: Head,
   header: Placeholder,
   html: RawHTML,
   image: Image,
   menu: Placeholder,
   'menu-item': Placeholder,
-  parsely: Parsely,
-  'social-links': SocialList,
-  'social-share': SocialList,
-  'social-item': SocialItem,
 };
 
 /**
---
+++ packages/integrations/babel.config.js
@@ -0,0 +1,5 @@
+const path = require('path');
+
+module.exports = {
+  extends: path.resolve('../../babel.config.base.js'),
+};
