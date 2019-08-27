1412108f281bba2af7a0d1fadaee3cf836fcdfdf
Pattie Reaves <pattie@alley.co>
2019-08-27 07:57:53 -0700

    MIT-115: Add image column.

--- components/topicHeader/index.js
+++ components/topicHeader/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'critical-style-loader/lib';
+import { findChildByName } from 'utils/children';
 
 // Styles
 import styles from './topicHeader.css';
 
-const TopicHeader = ({ name, description }) => (
-  <header className={styles.wrapper}>
-    <div className={styles.meta}>
-      <h1 className={styles.name}>{name}</h1>
-      <p className={styles.description}>{description}</p>
-    </div>
-  </header>
-);
+const TopicHeader = ({ name, description, children }) => {
+  const { image } = findChildByName('image', children);
+  return (
+    <header className={styles.wrapper}>
+      <div className={styles.meta}>
+        <h1 className={styles.name}>{name}</h1>
+        <p className={styles.description}>{description}</p>
+      </div>
+      <div className={styles.image}>{image}</div>
+    </header>
+  );
+};
 
 TopicHeader.propTypes = {
   description: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
 };
 
 export default withStyles(styles)(TopicHeader);
