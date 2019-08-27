3dc0bf89e53c59207fbe460f90a2b685dc0e921d
Matt <mattn@alley.co>
2019-08-27 20:14:37 +1000

    topics page article teaser component

--- components/termArchiveContentListItem/index.js
+++ components/termArchiveContentListItem/index.js
@@ -11,9 +11,34 @@ const TermArchiveContentListItem = ({
   console.log(permalink, excerpt);
   return (
     <div className={styles.wrapper}>
-      {title}
-      <p>Lorem ipsum </p>
-      {children}
+      <article className={styles.storyTease}>
+        <div className={styles.meta}>
+          <div className={styles.metaLeft}>
+            <a className={styles.eyebrowLink} href="https://technologyreview.com">
+              Climate Change
+            </a>
+            <time className={styles.timestamp}>Aug 26</time>
+          </div>
+          <div className={styles.shareMenu}>
+            <button
+              type="button"
+              aria-label="Open Share Menu"
+              className={styles.shareMenuToggle}
+            >
+              <div className={styles.dot} />
+              <div className={styles.dot} />
+              <div className={styles.dot} />
+            </button>
+          </div>
+        </div>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.excerpt}>At a major AI research conference,
+        one researcher laid out how existing AI techniques might be used
+        to analyze causal relationships in data.
+        </p>
+        <h4 className={styles.subTitle}>Welcome to the new chip race</h4>
+        {children}
+      </article>
     </div>
   );
 };
