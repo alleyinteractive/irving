7fc197ce098b302bb98d46f73c6f736c682db81a
Pattie Reaves <pattie@alley.co>
2019-09-09 07:51:03 -0700

    MIT-218: Remove commented out code.

--- components/listDescription/index.js
+++ components/listDescription/index.js
@@ -5,14 +5,12 @@ import { withStyles } from 'critical-style-loader/lib';
 // Styles
 import styles from './listDescription.css';
 
-// const [firstParagraph] = children;
-// const { content } = firstParagraph.props; // eslint-disable-line
-// debugger; // eslint-disable-line
 const ListDescription = ({ children, color }) => (
   <div className={styles.wrapper} style={{ '--highlight-color': color }}>
     {children}
   </div>
 );
+
 ListDescription.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
   color: PropTypes.string,
