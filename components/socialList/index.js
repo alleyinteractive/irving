e6e9faaa2770310c46cf15fa1018090e343f1286
Matt <mattn@alley.co>
2019-09-10 16:37:34 +1000

    adding theme for lists

--- components/socialList/index.js
+++ components/socialList/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'critical-style-loader/lib';
+import classNames from 'classnames';
+import withThemes from 'components/hoc/withThemes';
 import { __ } from '@wordpress/i18n';
 import styles from './socialList.css';
+import trListStyles from './trList.css';
 
 const SocialList = (props) => {
-  const { children: links } = props;
+  const { children: links, theme } = props;
   return (
-    <div className={styles.wrapper}>
-      <h3 className={styles.title}>
+    <div className={classNames(theme.wrapper, {})}>
+      <h3 className={theme.title}>
         {__('Share', 'mittr')}
       </h3>
-      <ul className={styles.list}>
+      <ul className={theme.list}>
         {links}
       </ul>
     </div>
@@ -23,8 +26,14 @@ SocialList.propTypes = {
    * Component children, usually a list of `<SocialItem />` components
    */
   children: PropTypes.node.isRequired,
+  theme: PropTypes.object.isRequired,
 };
 
-const wrapWithStyles = withStyles(styles);
+const wrapWithStyles = withStyles(styles, trListStyles);
 
-export default wrapWithStyles(SocialList);
+const wrapWithThemes = withThemes('list-header', {
+  default: styles,
+  trList: trListStyles,
+});
+
+export default wrapWithStyles(wrapWithThemes(SocialList));
