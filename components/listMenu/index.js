7fb335a00f88b0ea9eaa1bac0c75890a0ee4817b
Pattie Reaves <pattie@alley.co>
2019-09-10 19:52:37 -0700

    MIT-243: Allow themed menus.

--- components/listMenu/index.js
+++ components/listMenu/index.js
@@ -4,21 +4,37 @@ import { withStyles } from 'critical-style-loader/lib';
 import { UIDReset, UIDConsumer } from 'react-uid';
 import Link from 'components/helpers/link';
 import { __ } from '@wordpress/i18n';
+import classNames from 'classnames';
 
 // Styles
 import styles from './listMenu.css';
 
-const ListMenu = ({ children, title, permalink }) => (
+const ListMenu = ({
+  children, title, permalink, themeName,
+}) => (
   <UIDReset>
     <UIDConsumer>
       {(id, uid) => {
         const listID = uid('list-menu');
         return (
-          <nav className={styles.wrapper} aria-label={__('List', 'mittr')}>
+          <nav
+            className={classNames(styles.wrapper, themeName)}
+            aria-label={__('List', 'mittr')}
+          >
             <h2 id={listID}>
-              <Link to={permalink} className={styles.title}>{title}</Link>
+              <Link
+                to={permalink}
+                className={classNames(
+                  styles.title,
+                  styles[`title--${themeName}`]
+                )}
+              >
+                {title}
+              </Link>
             </h2>
-            <ul aria-labelledby={listID} className={styles.list}>{children}</ul>
+            <ul aria-labelledby={listID} className={styles.list}>
+              {children}
+            </ul>
           </nav>
         );
       }}
@@ -30,6 +46,11 @@ ListMenu.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
   permalink: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  themeName: PropTypes.string,
+};
+
+ListMenu.defaultProps = {
+  themeName: '',
 };
 
 export default withStyles(styles)(ListMenu);
