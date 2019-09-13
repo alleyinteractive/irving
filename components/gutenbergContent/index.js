3fc34e3a37523b2124bc3ef09a417fa2470a25ab
Pattie Reaves <pattie@pzunk.net>
2019-09-12 17:55:28 -0700

    MIT-117: Scaffold in feed items and gutenberg content.

---
+++ components/gutenbergContent/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from 'critical-style-loader/lib';
+import withThemes from 'components/hoc/withThemes';
+
+import styles from './gutenbergContent.css';
+import infeedTheme from './gutenbergContent--infeed.css';
+
+const GutenbergContent = ({ children, className, theme }) => (
+  <div className={classNames(theme.wrapper, className)}>
+    {children.map((child) => React.cloneElement(child, {
+      className: theme.content,
+      oembed: true,
+    }))}
+  </div>
+);
+
+GutenbergContent.defaultProps = {
+  className: '',
+};
+
+GutenbergContent.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  className: PropTypes.string,
+  theme: PropTypes.shape({
+    wrapper: PropTypes.string,
+    content: PropTypes.string,
+  }).isRequired,
+};
+
+export default withThemes('gutenberg-content', {
+  default: styles,
+  infeed: infeedTheme,
+})(withStyles(styles, infeedTheme)(GutenbergContent));
---
+++ components/inFeedItem/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'critical-style-loader/lib';
+import Link from 'components/helpers/link';
+import { __ } from '@wordpress/i18n';
+import { findChildByName } from 'utils/children';
+
+// Styles
+import styles from './inFeedItem.css';
+
+const feedEyebrow = ({ customEyebrow, topic, topicLink }) => {
+  if ('' === customEyebrow) {
+    return (
+      <Link className={styles.eyebrowLink} to={topicLink}>
+        {topic}
+      </Link>
+    );
+  }
+
+  return <div className={styles.eyebrow}>{customEyebrow}</div>;
+};
+
+feedEyebrow.propTypes = {
+  customEyebrow: PropTypes.string.isRequired,
+  topic: PropTypes.string.isRequired,
+  topicLink: PropTypes.string.isRequired,
+};
+
+const InFeedItem = ({
+  title,
+  children,
+  customEyebrow,
+  topic,
+  topicLink,
+  postDate,
+}) => {
+  const image = findChildByName('image', children);
+  const contentFooter = findChildByName('content-footer', children);
+
+  // Theme content block
+  const gutenbergContent = findChildByName('gutenberg-content', children);
+  const content = React.cloneElement(gutenbergContent, {
+    className: styles.content,
+    themeName: 'infeed',
+  });
+
+  return (
+    <article className={styles.wrapper}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>{title}</h1>
+        <div className={styles.meta}>
+          <div className={styles.eyebrow}>
+            <span className="screen-reader-text">
+              {__('Category: ', 'mittr')}
+            </span>
+            {feedEyebrow({ customEyebrow, topic, topicLink })}
+          </div>
+          <div className="postDate">
+            <span className="screen-reader-text">{__('Posted ', 'mittr')}</span>
+            {postDate}
+          </div>
+        </div>
+      </header>
+      {image && <div className={styles.image}>{image}</div>}
+      <div className={styles.content}>{content}</div>
+      {contentFooter}
+    </article>
+  );
+};
+
+InFeedItem.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  customEyebrow: PropTypes.string.isRequired,
+  postDate: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  topic: PropTypes.string.isRequired,
+  topicLink: PropTypes.string.isRequired,
+};
+
+export default withStyles(styles)(InFeedItem);
