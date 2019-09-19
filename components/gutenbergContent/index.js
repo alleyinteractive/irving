a760338edb36733075239950b1a8572c20445462
Pattie Reaves <pattie@pzunk.net>
2019-09-19 13:11:22 -0700

    MIT-253: Allow components to be prepended to the post content.

--- components/gutenbergContent/index.js
+++ components/gutenbergContent/index.js
@@ -3,18 +3,28 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from 'critical-style-loader/lib';
 import withThemes from 'components/hoc/withThemes';
+import { findChildByName } from 'utils/children';
 
 import styles from './gutenbergContent.css';
 import infeedTheme from './gutenbergContent--infeed.css';
 
-const GutenbergContent = ({ children, className, theme }) => (
-  <div className={classNames(theme.wrapper, className)}>
-    {children.map((child) => React.cloneElement(child, {
-      className: theme.content,
-      oembed: true,
-    }))}
-  </div>
-);
+const GutenbergContent = ({ children, className, theme }) => {
+  const html = findChildByName('html', children);
+  const contentToInsert = children.filter(
+    ({ props: { componentName } }) => 'html' !== componentName
+  );
+
+  return (
+    <div className={classNames(theme.wrapper, className)}>
+      {html &&
+        React.cloneElement(html, {
+          className: theme.content,
+          oembed: true,
+          prependChildren: contentToInsert,
+        })}
+    </div>
+  );
+};
 
 GutenbergContent.defaultProps = {
   className: '',
