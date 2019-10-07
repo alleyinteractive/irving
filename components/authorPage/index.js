492c27f0625e2a924e1f0bc3e3719607e4cc1061
Tyler Reckart <tyler@alley.co>
2019-10-07 09:17:42 -0400

    MIT-131: Update social profile list

--- components/authorPage/index.js
+++ components/authorPage/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'critical-style-loader/lib';
 import TwitterIcon from 'assets/icons/twitter.svg';
+import FacebookIcon from 'assets/icons/facebook.svg';
+import LinkedinIcon from 'assets/icons/linkedin.svg';
 import { findChildByName } from 'utils/children';
 import styles from './authorPage.css';
 
@@ -11,6 +13,8 @@ const AuthorPage = ({
   name,
   title,
   twitter,
+  facebook,
+  linkedin,
 }) => {
   const image = findChildByName('image', children);
   const bio = findChildByName('html', children);
@@ -39,12 +43,30 @@ const AuthorPage = ({
           <div className={styles.bio}>{bio}</div>
 
           {twitter && (
-            <div className={styles.twitter}>
-              <div className={styles.socialIcon}>
+            <a className={styles.socialLink} href={`https://twitter.com/${twitter}`}>
+              <div className={styles.twitterIcon}>
                 <TwitterIcon />
               </div>
               <span>{twitter}</span>
-            </div>
+            </a>
+          )}
+
+          {facebook && (
+            <a className={styles.socialLink} href={`https://facebook.com/${facebook}`}>
+              <div className={styles.facebookIcon}>
+                <FacebookIcon />
+              </div>
+              <span>{facebook}</span>
+            </a>
+          )}
+
+          {linkedin && (
+            <a className={styles.socialLink} href={`https://linkedin.com/in/${linkedin}`}>
+              <div className={styles.linkedinIcon}>
+                <LinkedinIcon />
+              </div>
+              <span>{linkedin}</span>
+            </a>
           )}
         </div>
       </div>
@@ -57,6 +79,8 @@ AuthorPage.propTypes = {
   name: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   twitter: PropTypes.string.isRequired,
+  facebook: PropTypes.string.isRequired,
+  linkedin: PropTypes.string.isRequired,
 };
 
 export default withStyles(styles)(AuthorPage);
