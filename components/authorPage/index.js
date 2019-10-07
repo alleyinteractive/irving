fabfca6abc6f62a612b7a5e67c76ddd39d463e86
Tyler Reckart <tyler@alley.co>
2019-10-07 11:22:34 -0400

    MIT-131: Fix footer rendering issues

--- components/authorPage/index.js
+++ components/authorPage/index.js
@@ -21,57 +21,55 @@ const AuthorPage = ({
   const bio = findChildByName('html', children);
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.profileWrapper}>
-        <div className={styles.imageWrapper}>
-          <div className={styles.profileImage}>{image}</div>
-          <span className={styles.title}>{title}</span>
-        </div>
+    <div className={styles.profileWrapper}>
+      <div className={styles.imageWrapper}>
+        <div className={styles.profileImage}>{image}</div>
+        <span className={styles.title}>{title}</span>
+      </div>
 
-        <div>
-          <div className={styles.profile}>
-            <div className={styles.mobileImageWrapper}>
-              {image}
-            </div>
-
-            <div className={styles.meta}>
-              <h2 className={styles.affiliation}>
-                {'staff' === type ? 'Our Team' : 'Contributor'}
-              </h2>
-              <h1 className={styles.name}>{name}</h1>
-              <span className={styles.title}>{title}</span>
-            </div>
+      <div>
+        <div className={styles.profile}>
+          <div className={styles.mobileImageWrapper}>
+            <div className={styles.profileImage}>{image}</div>
           </div>
 
-          <div className={styles.bio}>{bio}</div>
-
-          {twitter && (
-            <a className={styles.socialLink} href={`https://twitter.com/${twitter}`}>
-              <div className={styles.twitterIcon}>
-                <TwitterIcon />
-              </div>
-              <span>{twitter}</span>
-            </a>
-          )}
-
-          {facebook && (
-            <a className={styles.socialLink} href={`https://facebook.com/${facebook}`}>
-              <div className={styles.facebookIcon}>
-                <FacebookIcon />
-              </div>
-              <span>{facebook}</span>
-            </a>
-          )}
-
-          {linkedin && (
-            <a className={styles.socialLink} href={`https://linkedin.com/in/${linkedin}`}>
-              <div className={styles.linkedinIcon}>
-                <LinkedinIcon />
-              </div>
-              <span>{linkedin}</span>
-            </a>
-          )}
+          <div className={styles.meta}>
+            <span className={styles.authorType}>
+              {'staff' === type ? 'Our Team' : 'Contributor'}
+            </span>
+            <h1 className={styles.name}>{name}</h1>
+            <span className={styles.title}>{title}</span>
+          </div>
         </div>
+
+        <div className={styles.bio}>{bio}</div>
+
+        {twitter && (
+          <a className={styles.socialLink} href={`https://twitter.com/${twitter}`}>
+            <div className={styles.twitterIcon}>
+              <TwitterIcon />
+            </div>
+            <span>{twitter}</span>
+          </a>
+        )}
+
+        {facebook && (
+          <a className={styles.socialLink} href={`https://facebook.com/${facebook}`}>
+            <div className={styles.facebookIcon}>
+              <FacebookIcon />
+            </div>
+            <span>{facebook}</span>
+          </a>
+        )}
+
+        {linkedin && (
+          <a className={styles.socialLink} href={`https://linkedin.com/in/${linkedin}`}>
+            <div className={styles.linkedinIcon}>
+              <LinkedinIcon />
+            </div>
+            <span>{linkedin}</span>
+          </a>
+        )}
       </div>
     </div>
   );
