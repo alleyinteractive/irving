5131ad24db31b9dfc9ad7b8d33a23ceb56be44f3
Owen Stowe <ostowe@gmail.com>
2019-10-11 11:24:46 -0400

    Add comment and test cases for filepaths

--- utils/addTrailingSlash.test.js
+++ utils/addTrailingSlash.test.js
@@ -11,15 +11,23 @@ describe('addTrailingSlash', () => {
 
   it('should add a trailling slash to URLs without one', () => {
     expect(addTrailingSlash('/foo')).toBe('/foo/');
+    expect(addTrailingSlash('https://www.test.com/foo')).toBe('https://www.test.com/foo/');
   });
 
   it('should leave URLs with a trailing slash as-is', () => {
     expect(addTrailingSlash('/foo/')).toBe('/foo/');
+    expect(addTrailingSlash('https://www.test.com/foo/')).toBe('https://www.test.com/foo/');
+  });
+
+  it('should not add a trailling slash to a filepath', () => {
+    expect(addTrailingSlash('/assets/file.jpg')).toBe('/assets/file.jpg');
+    expect(addTrailingSlash('https://www.test.com/foo/file.jpg')).toBe('https://www.test.com/foo/file.jpg');
   });
 
   it('should do nothing if the appropriate ENV var is not set', () => {
     process.env.CONFIG_FORCE_TRAILING_SLASHES = false;
 
     expect(addTrailingSlash('/foo')).toBe('/foo');
+    expect(addTrailingSlash('https://www.test.com/foo')).toBe('https://www.test.com/foo');
   });
 });
