a2523fdecd82d10561b6606c5024f6f4b9d4e672
Owen Stowe <ostowe@gmail.com>
2019-10-21 22:52:15 -0400

    Switch to cntext for grid container/cell
    - bring in withThemes hoc for styled components

--- packages/styled-components/components/index.js
+++ packages/styled-components/components/index.js
@@ -1,8 +1,10 @@
 import CssReset from './reset';
+import withThemes from './withThemes';
 import { GridCell, GridContainer } from './grid';
 
 export {
   GridCell,
   GridContainer,
   CssReset,
+  withThemes,
 };
