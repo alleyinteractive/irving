1642adb637ce354ec3d3ae918dad1067fd5f6f43
Owen Stowe <ostowe@gmail.com>
2019-10-22 15:55:02 -0400

    Fix param order of pxToRelative

--- packages/styled-components/utils/pxToRelative.js
+++ packages/styled-components/utils/pxToRelative.js
@@ -1,5 +1,5 @@
 // Convert a pixel value to a relative one using a base font size.
-const pxToRelative = (value, unit, base) => (
+const pxToRelative = (value, base, unit) => (
   `${parseFloat(value) / base}${unit}`
 );
 
--- packages/styled-components/variables/breakpoints.js
+++ packages/styled-components/variables/breakpoints.js
@@ -1,7 +1,7 @@
 /**
  * Custom media queries
  */
-const bkptVal = {
+const breakpointValues = {
   xxl: 90,
   xl: 80,
   lg: 64,
@@ -9,21 +9,21 @@ const bkptVal = {
   sm: 32,
 };
 
-const breakpointObj = Object.keys(bkptVal)
+const breakpointObj = Object.keys(breakpointValues)
   .reduce((acc, curr) => {
-    acc[`${curr}Min`] = `min-width: ${bkptVal[curr]}rem`;
-    acc[`${curr}Max`] = `max-width: ${bkptVal[curr] - 0.0001}rem`;
-    acc[`${curr}Val`] = `${bkptVal[curr]}rem`;
+    acc[`${curr}Min`] = `min-width: ${breakpointValues[curr]}rem`;
+    acc[`${curr}Max`] = `max-width: ${breakpointValues[curr] - 0.0001}rem`;
+    acc[`${curr}Val`] = `${breakpointValues[curr]}rem`;
     return acc;
   }, {});
 
 const breakpoints = {
   adminBarMedMin: 'min-width: 783px',
   adminBarSmMin: 'min-width: 601px',
-  bkptVal,
+  breakpointValues,
   ...breakpointObj,
 };
 
-module.exports.bkptVal = bkptVal;
+module.exports.breakpointValues = breakpointValues;
 module.exports.breakpointNames = Object.keys(breakpoints).concat('all');
 module.exports.breakpoints = breakpoints;
