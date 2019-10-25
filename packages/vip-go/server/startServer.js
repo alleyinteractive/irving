c3674d21a69342dd8349c135cbcf34a6f90aa3a7
Owen Stowe <ostowe@gmail.com>
2019-10-25 11:39:21 -0400

    Rename createServer to startServer, start the server in that function

---
+++ packages/vip-go/server/startServer.js
@@ -0,0 +1,19 @@
+/* eslint-disable global-require, no-console */
+const { server } = require('@automattic/vip-go');
+
+const {
+  PORT = 3001,
+  NODE_ENV,
+} = process.env;
+
+/* eslint-disable global-require, no-console, import/order */
+module.exports = function createServer(app) {
+  let vipServer;
+
+  if ('development' !== NODE_ENV) {
+    vipServer = server(app, { PORT });
+    vipServer.listen();
+  }
+
+  return vipServer;
+};
