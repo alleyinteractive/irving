7f5bbf9fe6e0817e5802083eab20e188cb146aa8
Owen Stowe <ostowe@gmail.com>
2019-10-25 11:49:37 -0400

    Fix createServer imports in vip go package

--- packages/core/server/startServer.js
+++ packages/core/server/startServer.js
@@ -7,7 +7,7 @@ const {
 } = process.env;
 
 /* eslint-disable global-require, no-console, import/order */
-module.exports = function createServer(app) {
+module.exports = function startServer(app) {
   let server;
 
   if (HTTPS_KEY_PATH && HTTPS_CERT_PATH) {
