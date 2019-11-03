eb32d7fab862fd3d41a1c521ee0ed9b73f65d285
Pattie Reaves <pattie@alley.co>
2019-11-03 14:43:56 -0700

    MIT-350: Add todo/tutorial link.

--- sagas/userSaga.js
+++ sagas/userSaga.js
@@ -5,15 +5,16 @@ import nexusService from 'services/nexusService';
 
 const debug = createDebug('sagas:login');
 
+// @todo needs to handle more than just someone requesting login.
+// I found this tutorial helpful: https://github.com/sotojuan/saga-login-flow
 export default function* loginFlow(data) {
   const {
     payload: { email },
   } = data;
 
-  // Make the request to connect to Nexus here.
   try {
+    // Call Nexus
     const response = yield call(nexusService.getAccount, email);
-
     yield put(actionReceiveUserLogin(response));
   } catch (error) {
     yield call(debug, error);
