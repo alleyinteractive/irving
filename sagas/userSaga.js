5db15f7b4b23f44f1eece4dd5fb4b5d3e4a14239
Pattie Reaves <pattie@pzunk.net>
2019-11-03 13:46:59 -0700

    MIT-350: Working integration with the development server.

--- sagas/userSaga.js
+++ sagas/userSaga.js
@@ -1,6 +1,7 @@
 import { call, put } from 'redux-saga/effects';
 import { actionReceiveUserLogin } from 'actions/userActions';
 import createDebug from 'services/createDebug';
+import nexusService from 'services/nexusService';
 
 const debug = createDebug('sagas:login');
 
@@ -11,10 +12,7 @@ export default function* loginFlow(data) {
 
   // Make the request to connect to Nexus here.
   try {
-    const response = yield call(async (userEmail) => {
-      console.log(`${userEmail} is trying to login.`); // eslint-disable-line
-      return true;
-    }, email);
+    const response = yield call(nexusService.getAccount, email);
 
     yield put(actionReceiveUserLogin(response));
   } catch (error) {
---
+++ services/nexusService.js
@@ -0,0 +1,30 @@
+export default {
+  // @todo write authentication to nexus production server.
+
+  /**
+   * Lookup an account by email address.
+   *
+   * @param {string} email User email to look up.
+   * @returns {{}}
+   */
+  async getAccount(email) {
+    const endpoint = 'http://localhost:5000';
+    // const authorization = 'test';
+
+    try {
+      const response = await fetch(`${endpoint}/api/user/email/${email}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'test',
+        },
+        credentials: 'include',
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.info('There was a problem.', error); // eslint-disable-line no-console
+    }
+    return {};
+  },
+};
