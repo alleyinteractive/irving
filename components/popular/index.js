63b8920e9dea0c80b39be84d4236669a1fea0c7b
Allan Collins <allan@alley.co>
2019-11-05 10:51:12 -0700

    Popular component.

---
+++ components/popular/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'critical-style-loader/lib';
+import withThemes from 'components/hoc/withThemes';
+import styles from './popular.css';
+import Link from '../helpers/link';
+import inFeedStyles from './inFeed.css';
+
+const Popular = ({ popular, theme }) => (
+  <div className={theme.popular}>
+    <h3 className={theme.title}>Popular</h3>
+    <ul className={theme.stories}>
+      {popular.map((item) => (
+        <li className={theme.story} key={item.title}>
+          <Link to={item.link}>{item.title}</Link>
+          <br />
+          <span className={theme.byline}>
+            {item.authors.map(
+              (author) => (
+                { author }
+              )
+            )}
+          </span>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+Popular.propTypes = {
+  popular: PropTypes.array.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withThemes('popular', {
+  default: styles,
+  'in-feed': inFeedStyles,
+})(withStyles(styles, inFeedStyles)(Popular));
