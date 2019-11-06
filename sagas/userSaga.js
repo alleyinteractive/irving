fe73cd24ce4c446a8ba12c65c2ac4852d1978992
Tyler Reckart <tyler@alley.co>
2019-11-06 09:37:11 -0500

    MIT-348: Authorize fn

--- sagas/userSaga.js
+++ sagas/userSaga.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
 import { call, put, select } from 'redux-saga/effects';
 import {
   actionReceiveUserLogin,
   actionRequestAuth,
   actionReceiveUserAuth,
 } from 'actions/userActions';
-import { authSelector, isValid, validTo } from 'selectors/getAuth';
+import {
+  authSelector,
+  isValid as isAuthValid,
+  validTo,
+} from 'selectors/getAuth';
 import createDebug from 'services/createDebug';
 import nexusService from 'services/nexusService';
 
@@ -19,11 +22,39 @@ export default function* loginFlow(data) {
   } = data;
 
   try {
-    const { header } = yield select(authSelector);
+    const { header } = yield call(authorize);
+
     const response = yield call(nexusService.getAccount, { email, header });
     yield put(actionReceiveUserLogin(response));
   } catch (error) {
     yield call(debug, error);
   }
+}
+
+// @todo implement me in the login flow.
+export function* authorize() {
+  const isValid = yield select(isAuthValid);
+  const timestamp = yield select(validTo);
+  const timeLimit = Math.floor(Date.now() / 1000) - 300;
+
+  try {
+    if (false === isValid || timestamp > timeLimit) {
+      yield put(actionRequestAuth());
+
+      const auth = yield call(nexusService.getAuth);
+      if (false !== auth.isValid) {
+        yield put(actionReceiveUserAuth(auth));
+      } else {
+        // @todo define error state.
+        throw new Error('Request failed', auth);
+      }
+
+      return auth;
+    }
+  } catch (error) {
+    console.info('There was a problem while requesting authorization.', error); // eslint-disable-line no-console
+  }
+
+  return yield select(authSelector);
 }
 
