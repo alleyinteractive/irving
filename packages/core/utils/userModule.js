df1b7829ecb555656cbb7b5a7261c4765b6cb8cc
Owen Stowe <ostowe@gmail.com>
2019-11-06 19:54:23 -0500

    Add maybeResolveBuildModule

--- packages/core/utils/userModule.js
+++ packages/core/utils/userModule.js
@@ -4,9 +4,28 @@ const {
   appRoot,
   buildContext,
   appIrvingRoot,
+  irvingRoot,
 } = require('../config/paths');
 
 /* eslint-disable import/no-dynamic-require, global-require */
+/**
+ * Resolve the path to a user module, falling back to Irving core version.
+ *
+ * @param {string} userPath Path to user-defined module, relative to user app root.
+ * @param {string} corePath Path to Irving core module, relative to Irving core root, if different from user path.
+ */
+module.exports.maybeResolveBuildModule = (userPath, corePath) => {
+  const defaultPath = corePath || userPath;
+
+  // Resolve file relative to build context if it exists.
+  if (fs.existsSync(path.resolve(buildContext, userPath))) {
+    return path.resolve(buildContext, userPath);
+  }
+
+  // Resolve file relative to irving app in build context otherwise.
+  return path.resolve(irvingRoot, defaultPath);
+};
+
 /**
  * Resolve the path to a user module, falling back to Irving core version.
  *
