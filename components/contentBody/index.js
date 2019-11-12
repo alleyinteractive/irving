ce6ad46cd6b702338e10da0c1f809a7b8eb37eb1
Tyler Reckart <tyler@alley.co>
2019-11-12 11:17:45 -0500

    MIT-240: Tweaks/improvements

--- components/contentBody/index.js
+++ components/contentBody/index.js
@@ -1,44 +1,73 @@
 /* eslint-disable */
-import React, { useState, useEffect } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+} from 'react';
 import PropTypes from 'prop-types';
 import { __ } from '@wordpress/i18n';
 import { withStyles } from 'critical-style-loader/lib';
 import styles from './contentBody.css';
 
 const ContentBody = (props) => {
-  const [truncateContent, setTruncationValue] = useState(false);
+  const [truncateContent, setTruncation] = useState(true);
+  const [contentHeight, setContentHeight] = useState(400);
+  const contentRef = useRef();
+
   const {
     children,
     truncatedCTA,
     wordCount
   } = props;
+
   useEffect(() => {
     const { referrer } = document;
     const { location: { origin } } = window;
 
     if (referrer !== origin) {
-      setTruncationValue(true);
+      setTruncation(true);
+    } else {
+      showFullText();
     }
-  });
+  }, truncateContent);
 
-  if (truncateContent) {
-    return (
-      <div className={styles.wrapper}>
-        <div className={styles.truncatedContent}>{children}</div>
+  const showFullText = () => {
+    // Remove the truncation button and height limit.
+    setTruncation(false);
+    // Update the container's height to be that of the content.
+    setContentHeight(
+      contentRef.current.getBoundingClientRect().height
+    );
+  }
 
+  return (
+    <div className={styles.wrapper}>
+      <div
+        className={styles.overlay}
+        style={{ display: contentHeight === 400 ? 'block' : 'none' }}
+      />
+
+      <div
+        className={contentHeight === 400 ? styles.contentHidden : ''}
+        style={{ height: contentHeight }}
+      >
+        <div ref={contentRef}>
+          {children}
+        </div>
+      </div>
+
+      {truncateContent && (
         <button
           className={styles.truncationButton}
           type="button"
-          onClick={() => setTruncationValue(false)}
+          onClick={showFullText}
         >
           <strong>{__(`${truncatedCTA}`, 'mittr')}</strong>{' '}
           {__(`${wordCount} words`, 'mittr')}
         </button>
-      </div>
-    );
-  }
-
-  return <div className={styles.wrapper}>{children}</div>;
+      )}
+    </div>
+  );
 };
 
 ContentBody.propTypes = {
