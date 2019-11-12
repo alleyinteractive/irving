c33c4e81dd980968dba84f1e1e98003ece5e95ee
Tyler Reckart <tyler@alley.co>
2019-11-12 10:35:04 -0500

    MIT-240: Truncate body when coming from outside

--- components/contentBody/index.js
+++ components/contentBody/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { __ } from '@wordpress/i18n';
 import { withStyles } from 'critical-style-loader/lib';
 import styles from './contentBody.css';
 
 const ContentBody = (props) => {
-  const { children } = props;
+  const [truncateContent, setTruncationValue] = useState(false);
+  const {
+    children,
+    truncatedCTA,
+    wordCount
+  } = props;
+  useEffect(() => {
+    const { referrer } = document;
+    const { location: { origin } } = window;
+
+    if (referrer !== origin) {
+      setTruncationValue(true);
+    }
+  });
+
+  if (truncateContent) {
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.truncatedContent}>{children}</div>
+
+        <button
+          className={styles.truncationButton}
+          type="button"
+          onClick={() => setTruncationValue(false)}
+        >
+          <strong>{__(`${truncatedCTA}`, 'mittr')}</strong>{' '}
+          {__(`${wordCount} words`, 'mittr')}
+        </button>
+      </div>
+    );
+  }
 
   return <div className={styles.wrapper}>{children}</div>;
 };
