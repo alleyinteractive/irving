58f3610ce62c459fb99dd76cf64ebe272755ab7d
Tyler Reckart <tyler@alley.co>
2019-11-13 11:21:36 -0500

    MIT-145: Fix unit position on scroll

--- components/popular/index.js
+++ components/popular/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'critical-style-loader/lib';
 import Link from '../helpers/link';
@@ -8,32 +9,65 @@ import withThemes from '../hoc/withThemes';
 import styles from './popular.css';
 import inFeedStyles from './inFeed.css';
 
-const Popular = ({ popular, theme, themeName }) => (
-  <div className={theme.wrapper}>
-    <h3 className={theme.title}>Popular</h3>
-    <ul className={theme.stories}>
-      {popular.map((item, index) => (
-        <li className={theme.story} key={item.title}>
-          { 'in-feed' !== themeName && (
-            <div className={theme.itemCount}>
-              0{index + 1}.
-            </div>
-          )}
-          <Link to={item.link}>{item.title}</Link>
-          <br />
-          { 'in-feed' === themeName && (
-            <span className={theme.byline}>
-              {item.authorLink && (
-                <Link to={item.authorLink}>{item.author}</Link>
+const Popular = ({ popular, theme, themeName }) => {
+  const [isFixed, setFixedPosition] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      const node = document.getElementById('sticky__content-module');
+      const nodeOffset = node.getBoundingClientRect().top;
+      console.log(100 <= nodeOffset);
+
+      if (100 >= nodeOffset) {
+        setFixedPosition(true);
+      }
+      
+      if (100 <= nodeOffset) {
+        setFixedPosition(false);
+      }
+    });
+  }, isFixed);
+
+  console.log(isFixed);
+
+  return (
+    <div
+      className={theme.wrapper}
+      id="sticky__content-module"
+      style={{
+        position: isFixed ? 'fixed' : 'relative',
+        top: 82,
+      }}
+    >
+      {/* @todo consider moving ad placeholder to its own component. */}
+      <div className={styles.leaderboard}>Ad unit placeholder</div>
+      <div className={theme.wrapper}>
+        <h3 className={theme.title}>Popular</h3>
+        <ul className={theme.stories}>
+          {popular.map((item, index) => (
+            <li className={theme.story} key={item.title}>
+              { 'in-feed' !== themeName && (
+                <div className={theme.itemCount}>
+                  0{index + 1}.
+                </div>
               )}
-              {! item.authorLink ? item.author : ''}
-            </span>
-          )}
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+              <Link to={item.link}>{item.title}</Link>
+              <br />
+              { 'in-feed' === themeName && (
+                <span className={theme.byline}>
+                  {item.authorLink && (
+                    <Link to={item.authorLink}>{item.author}</Link>
+                  )}
+                  {! item.authorLink ? item.author : ''}
+                </span>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 Popular.propTypes = {
   popular: PropTypes.array.isRequired,
