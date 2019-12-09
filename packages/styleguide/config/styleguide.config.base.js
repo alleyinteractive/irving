d6fac4e597eae44ff3026bb688789ff03b51eea6
Owen Stowe <ostowe@gmail.com>
2019-12-09 11:32:56 -0500

    Remove acorn dependency

--- packages/styleguide/config/styleguide.config.base.js
+++ packages/styleguide/config/styleguide.config.base.js
@@ -20,7 +20,9 @@ module.exports = {
     // Recreate aliases as we can't use the same babel aliases for styleguide
     resolve: {
       alias: {
-        // Make all aliases absolute.
+        // Fix issue with acorn parser import.
+        acorn: require.resolve('acorn/dist/acorn.js'),
+        // Make all aliases absolute appRoot.
         ...Object.keys(aliases).reduce(
           (acc, alias) => (
             [...acc, path.join(irvingRoot, alias)]
