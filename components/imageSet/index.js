903654f6f3dc2af4aa68544d80771fa433024150
Eric Nation <eric.nation@gmail.com>
2019-12-09 13:53:22 -0600

    MIT-443: Remove unused prop

--- components/imageSet/index.js
+++ components/imageSet/index.js
@@ -5,19 +5,14 @@ import { withStyles } from 'critical-style-loader/lib';
 import styles from './imageSet.css';
 
 // eslint-disable-next-line no-unused-vars
-const ImageSet = ({ children, layout }) => (
+const ImageSet = ({ children }) => (
   <div className="imageSet__wrap">
     {children}
   </div>
 );
 
-ImageSet.defaultProps = {
-  layout: 'diptych',
-};
-
 ImageSet.propTypes = {
   children: PropTypes.arrayOf(PropTypes.node).isRequired,
-  layout: PropTypes.string,
 };
 
 export default withStyles(styles)(ImageSet);
