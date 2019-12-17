a953f66cca44bc352a8d431a54d9e0fc248e7f30
Pattie Reaves <pattie@alley.co>
2019-12-17 15:13:34 -0700

    MIT-370: Add more changes from vip-go irving package.

--- services/cacheService.js
+++ services/cacheService.js
@@ -1,9 +1,3 @@
-let service;
-const defaultService = {
-  get: () => null,
-  set: () => {},
-};
-
 /**
  * @typedef {object} CacheService
  * @property {function} get
@@ -13,34 +7,37 @@ const defaultService = {
  * @returns {CacheService}
  */
 const getService = () => {
-  // Memoize client connection, so it can reused.
-  if (service) {
-    return service;
-  }
-
-  // Redis env variables have not been configured.
-  if (! process.env.REDIS_URL) {
-    return defaultService;
-  }
-
   // We need to be explicit that redis is only imported when not executing
   // within a browser context, so that webpack can ignore this execution path
   // while compiling.
   if (! process.env.BROWSER) {
+    // Redis env variables have not been configured.
+    if (! process.env.REDIS_MASTER) {
+      return false;
+    }
+
     let Redis;
     // Check if optional redis client is installed.
     try {
       Redis = require('ioredis'); // eslint-disable-line global-require
     } catch (err) {
-      return defaultService;
+      return false;
     }
 
-    const client = new Redis(process.env.REDIS_URL);
+    const [host, port] = (process.env.REDIS_MASTER).split(':');
+    const opts = { host, port };
+
+    // Add password, if configured
+    if (process.env.REDIS_PASSWORD) {
+      opts.password = process.env.REDIS_PASSWORD;
+    }
+
+    const client = new Redis(opts);
     client.on('error', (err) => {
       console.error(err); // eslint-disable-line no-console
     });
 
-    service = {
+    return {
       client,
       async get(key) {
         return JSON.parse(await this.client.get(key));
@@ -50,15 +47,16 @@ const getService = () => {
           key,
           JSON.stringify(value),
           'EX',
-          process.env.CACHE_EXPIRE || 300
+          600
         );
       },
+      del(key) {
+        return this.client.del(key);
+      },
     };
-
-    return service;
   }
 
-  return defaultService;
+  return false;
 };
 
-export default getService;
+module.exports = getService;
