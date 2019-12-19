2b3dc467c8220239a54015f8f7b2846543ad3228
Tyler Reckart <tyler@alley.co>
2019-12-19 12:19:53 -0500

    MIT-324: Update column width

--- components/contentList/index.js
+++ components/contentList/index.js
@@ -3,12 +3,10 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'critical-style-loader/lib';
 import classNames from 'classnames';
 import { UIDConsumer } from 'react-uid';
-import parse from 'html-react-parser';
 import styles from './contentList.css';
 
 const ContentList = ({
   align,
-  className,
   listTitle,
   children,
 }) => 0 < children.length && (
@@ -24,13 +22,8 @@ const ContentList = ({
           })}
         >
           {listTitle && (
-            <h2
-              className={classNames('contentList__header', {
-                'company-feature': 'company-feature' === className,
-              })}
-              id={titleID}
-            >
-              {parse(listTitle)}
+            <h2 className="contentList__header" id={titleID}>
+              {listTitle}
             </h2>
           )}
           <ul className="contentList__listWrap" aria-labelledby={titleID}>
@@ -45,12 +38,10 @@ const ContentList = ({
 ContentList.defaultProps = {
   align: '',
   listTitle: '',
-  className: '',
   children: [],
 };
 
 ContentList.propTypes = {
-  className: PropTypes.string,
   listTitle: PropTypes.string,
   align: PropTypes.string,
   children: PropTypes.arrayOf(PropTypes.element),
