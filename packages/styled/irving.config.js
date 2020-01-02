d064f45f0710ccd8ad8b7abf16baefb52b20327e
Owen Stowe <ostowe@gmail.com>
2020-01-02 15:13:26 -0500

    Remove unnecessary config fields
    - update audio player readme
    - fix getConfigField so it doesn't require user to create an irving.config.js
    - remove test packages from postcss package deps

--- packages/styled/irving.config.js
+++ packages/styled/irving.config.js
@@ -1,10 +1,8 @@
 import getAppTemplateVars from './server/getAppTemplateVars';
-import * as variables from './variables';
 
 // Export config
 export default {
   name: 'styled',
   getAppTemplateVars,
-  cssVariables: variables,
   styleguideConfig: './config/styleguide.config.js',
 };
--- packages/styled/irving.config.server.js
+++ packages/styled/irving.config.server.js
@@ -1,10 +1,8 @@
 const babelConfig = require('./config/getBabelConfig.js');
 const webpackConfig = require('./config/getWebpackConfig.js');
-const variables = require('./variables');
 
 module.exports = {
   name: 'styled',
   babelConfig,
   webpackConfig,
-  cssVariables: variables,
 };
