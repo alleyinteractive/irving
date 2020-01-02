35ec3ccef56d3867446e096a5b59053b0e27ca59
Owen Stowe <ostowe@gmail.com>
2020-01-02 16:16:35 -0500

    Fix aliases using resolve, not require
    - Add backups just in case in getConfigField

--- packages/core/utils/getConfigField.js
+++ packages/core/utils/getConfigField.js
@@ -4,14 +4,14 @@ let config;
 
 /* eslint-disable import/no-dynamic-require, global-require */
 if (process.env.BUILD) {
-  config = require('@irvingjs/irving.config').default;
+  config = require('@irvingjs/irving.config').default || {};
 } else {
   const { serverConfig: serverConfigPath } = require('../config/paths');
 
   // Wrap require for server config in try/catch to ensure things will work
   // if user decides not to create a server config.
   try {
-    config = require(serverConfigPath);
+    config = require(serverConfigPath) || {};
   } catch (e) {
     config = {};
   }
