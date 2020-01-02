779b28dd46b1bf330c39e45ade5da6f014905783
Pattie Reaves <pattie@alley.co>
2020-01-02 09:18:24 -0700

    MIT-477: Feed area column styling.

--- components/columnArea/index.js
+++ components/columnArea/index.js
@@ -11,51 +11,51 @@ import magazineTheme from './columnArea--magazine.css';
 import oneColumnTheme from './columnArea--oneCol.css';
 import pageTheme from './columnArea--page.css';
 
-const ColumnArea = ({ children, theme, themeName }) => {
+const ColumnArea = ({ children, theme }) => {
   // @todo leave this block for deprecation purposes (topics page, etc.)
   // It may make sense to refactor this component into multiple components.
-  if ('skinny-column' === themeName) {
-    // Separate content and sidebar
-    const content = children.filter(
-      (child) => 'sidebar' !== child.props.componentName
-    );
-    const sidebar = findChildByName('sidebar', children);
-
-    return (
-      <div className={theme.wrapper}>
-        {content && (
-          <div
-            className={theme.main}
-          >
-            {content}
-          </div>
-        )}
-        {sidebar && (
-          <div
-            className={theme.sidebar}
-          >
-            {sidebar}
-          </div>
-        )}
-      </div>
-    );
-  }
+  // if ('feed' === themeName) {
+  // Separate content and sidebar
+  const content = children.filter(
+    (child) => 'sidebar' !== child.props.componentName
+  );
+  const sidebar = findChildByName('sidebar', children);
 
   return (
     <div className={theme.wrapper}>
-      {children.map((child) => (
-        <div className={theme.column} key={child.props.componentName}>
-          {child}
+      {content && (
+        <div
+          className={theme.main}
+        >
+          {content}
         </div>
-      ))}
+      )}
+      {sidebar && (
+        <div
+          className={theme.sidebar}
+        >
+          {sidebar}
+        </div>
+      )}
     </div>
   );
+  // }
+
+  // return (
+  //   <div className={theme.wrapper}>
+  //     {children.map((child) => (
+  //       <div className={theme.column} key={child.props.componentName}>
+  //         {child}
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
 };
 
 ColumnArea.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
   theme: PropTypes.object.isRequired,
-  themeName: PropTypes.string.isRequired,
+  // themeName: PropTypes.string.isRequired,
 };
 
 export default withThemes('column-area', {
