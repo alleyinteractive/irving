564c33752179196d8ac2e94195db62ea13f3a9d3
Pattie Reaves <pattie@alley.co>
2020-01-02 21:37:07 -0700

    MIT-477: Fully style feed and full story themes.

--- components/columnArea/index.js
+++ components/columnArea/index.js
@@ -6,15 +6,12 @@ import { findChildByName } from 'utils/children';
 
 // Themes
 import feedColumnTheme from './columnArea--feed.css';
-import fullStoryTheme from './columnArea--full.css';
+import fullStoryTheme from './columnArea--fullStory.css';
 import magazineTheme from './columnArea--magazine.css';
 import oneColumnTheme from './columnArea--oneCol.css';
 import pageTheme from './columnArea--page.css';
 
 const ColumnArea = ({ children, theme }) => {
-  // @todo leave this block for deprecation purposes (topics page, etc.)
-  // It may make sense to refactor this component into multiple components.
-  // if ('feed' === themeName) {
   // Separate content and sidebar
   const content = children.filter(
     (child) => 'sidebar' !== child.props.componentName
@@ -39,29 +36,17 @@ const ColumnArea = ({ children, theme }) => {
       )}
     </div>
   );
-  // }
-
-  // return (
-  //   <div className={theme.wrapper}>
-  //     {children.map((child) => (
-  //       <div className={theme.column} key={child.props.componentName}>
-  //         {child}
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
 };
 
 ColumnArea.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
   theme: PropTypes.object.isRequired,
-  // themeName: PropTypes.string.isRequired,
 };
 
 export default withThemes('column-area', {
   default: feedColumnTheme,
   feed: feedColumnTheme,
-  story: fullStoryTheme,
+  fullStory: fullStoryTheme,
   single: oneColumnTheme,
   page: pageTheme,
   magazine: magazineTheme,
