37f956b5e09043a76d5d06cded08ce47a6688b34
Tyler Reckart <tyler@alley.co>
2020-01-13 11:51:57 -0500

    MIT-278: Configure front-end for content feed

--- components/authorPage/index.js
+++ components/authorPage/index.js
@@ -6,6 +6,7 @@ import TwitterIcon from 'assets/icons/twitter.svg';
 import FacebookIcon from 'assets/icons/facebook.svg';
 import LinkedinIcon from 'assets/icons/linkedin.svg';
 import { findChildByName } from 'utils/children';
+import { __ } from '@wordpress/i18n';
 import styles from './authorPage.css';
 
 const AuthorPage = ({
@@ -16,62 +17,71 @@ const AuthorPage = ({
   twitter,
   facebook,
   linkedin,
+  postCount,
 }) => {
   const image = findChildByName('image', children);
   const bio = findChildByName('html', children);
 
   return (
-    <div className={styles.profileWrapper}>
-      <div className={styles.imageWrapper}>
-        <div className={styles.profileImage}>{image}</div>
-        <span className={styles.title}>{title}</span>
-      </div>
-
-      <div>
-        <div className={styles.profile}>
-          <div className={styles.mobileImageWrapper}>
-            <div className={styles.profileImage}>{image}</div>
-          </div>
-
-          <div className={styles.meta}>
-            <span className={styles.authorType}>
-              {'staff' === type ? 'Our Team' : 'Contributor'}
-            </span>
-            <h1 className={styles.name}>{name}</h1>
-            <span className={styles.title}>{title}</span>
-          </div>
+    <React.Fragment>
+      <div className={styles.profileWrapper}>
+        <div className={styles.imageWrapper}>
+          <div className={styles.profileImage}>{image}</div>
+          <span className={styles.title}>{title}</span>
         </div>
 
-        <div className={styles.bio}>{bio}</div>
-
-        {twitter && (
-          <a className={styles.socialLink} href={`https://twitter.com/${twitter}`}>
-            <div className={styles.twitterIcon}>
-              <TwitterIcon />
+        <div>
+          <div className={styles.profile}>
+            <div className={styles.mobileImageWrapper}>
+              <div className={styles.profileImage}>{image}</div>
             </div>
-            <span>{twitter}</span>
-          </a>
-        )}
 
-        {facebook && (
-          <a className={styles.socialLink} href={`https://facebook.com/${facebook}`}>
-            <div className={styles.facebookIcon}>
-              <FacebookIcon />
+            <div className={styles.meta}>
+              <span className={styles.authorType}>
+                {'staff' === type ? 'Our Team' : 'Contributor'}
+              </span>
+              <h1 className={styles.name}>{name}</h1>
+              <span className={styles.title}>{title}</span>
             </div>
-            <span>{facebook}</span>
-          </a>
-        )}
+          </div>
 
-        {linkedin && (
-          <a className={styles.socialLink} href={`https://linkedin.com/in/${linkedin}`}>
-            <div className={styles.linkedinIcon}>
-              <LinkedinIcon />
-            </div>
-            <span>{linkedin}</span>
-          </a>
-        )}
+          <div className={styles.bio}>{bio}</div>
+
+          {twitter && (
+            <a className={styles.socialLink} href={`https://twitter.com/${twitter}`}>
+              <div className={styles.twitterIcon}>
+                <TwitterIcon />
+              </div>
+              <span>{twitter}</span>
+            </a>
+          )}
+
+          {facebook && (
+            <a className={styles.socialLink} href={`https://facebook.com/${facebook}`}>
+              <div className={styles.facebookIcon}>
+                <FacebookIcon />
+              </div>
+              <span>{facebook}</span>
+            </a>
+          )}
+
+          {linkedin && (
+            <a className={styles.socialLink} href={`https://linkedin.com/in/${linkedin}`}>
+              <div className={styles.linkedinIcon}>
+                <LinkedinIcon />
+              </div>
+              <span>{linkedin}</span>
+            </a>
+          )}
+        </div>
       </div>
-    </div>
+
+      {0 < postCount && (
+        <div className={styles.postCountWrapper}>
+          {__(`${postCount} posts by`, 'mittr')} <strong>{name}</strong>
+        </div>
+      )}
+    </React.Fragment>
   );
 };
 
@@ -79,6 +89,7 @@ AuthorPage.defaultProps = {
   twitter: '',
   facebook: '',
   linkedin: '',
+  postCount: 0,
 };
 
 AuthorPage.propTypes = {
@@ -89,6 +100,7 @@ AuthorPage.propTypes = {
   twitter: PropTypes.string,
   facebook: PropTypes.string,
   linkedin: PropTypes.string,
+  postCount: PropTypes.number,
 };
 
 export default withStyles(styles)(AuthorPage);
