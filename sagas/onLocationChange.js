7f28adb99e7ed19249524d7a19144d6e6c259a46
Tyler Reckart <tyler@alley.co>
2020-01-20 09:50:41 -0500

    MIT-552: receive authorization header

--- sagas/onLocationChange.js
+++ sagas/onLocationChange.js
@@ -1,13 +1,27 @@
-import { select } from 'redux-saga/effects';
+import { call, select } from 'redux-saga/effects';
 import getRouteMeta from 'selectors/getRouteMeta';
+import zephrService from 'services/zephrService';
 
 export default function* onLocationChange() {
   const { path } = yield select(getRouteMeta);
 
   switch (path) {
     case '/login':
-      console.log('login route hit');
+      yield call(requestLoginHeader);
       break;
     default:
   }
+}
+
+function* requestLoginHeader() {
+  const params = {
+    method: 'GET',
+    path: '/v3/forms/login',
+    body: '',
+  };
+
+  const header = yield call(zephrService.getRequestHeader, params);
+  console.log(header);
+
+  return header;
 }
--- services/zephrService.js
+++ services/zephrService.js
@@ -1,16 +1,23 @@
+/* eslint-disable */
 export default {
   /**
    * Retrieve the header used to as an authorization token to authenticate
    * Zephr requests.
    *
-   * @param {string} body    A stringified version of the request's body.
-   * @param {string} method  The request's method (e.g. "GET", "POST", etc).
-   * @param {string} path    The request's path (less the host e.g. /v3/users).
+   * @param {string} params Request parameters.
+   *
+   * @return {string} header The request header.
    */
-  async getRequestHeader(body, method, path) {
+  async getRequestHeader(params) {
     try {
+      const {
+        body,
+        method,
+        path
+      } = params;
+      const bodyParam = 0 < body.length ? `request_body=${body}&` : '';
       const request = await fetch(
-        `${process.env.API_ROOT_URL}/data/zephr_auth?request_body=${body}&method=${method}&path=${path}`,
+        `${process.env.API_ROOT_URL}/data/zephr_auth?${bodyParam}method=${method}&path=${path}`, // eslint-disable-line max-len
       );
       const header = await request.json();
 
