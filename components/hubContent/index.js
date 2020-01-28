513131f7d3f06ef1b17aff7456668b3fb246adb4
Gabriel Luethje <gabriel@alley.co>
2020-01-27 17:14:46 -0700

    Basic styling for content area

--- components/hubContent/index.js
+++ components/hubContent/index.js
@@ -2,23 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'critical-style-loader/lib';
 import { findChildByName } from 'utils/children';
+import Link from 'components/helpers/link';
 // Styles
 import styles from './hubContent.css';
 
-const HubContent = ({ children, title }) => {
+const HubContent = ({
+  children,
+  title,
+  sponsorName,
+  sponsorUrl,
+}) => {
   const content = findChildByName('html', children);
+  const sponsorLogo = findChildByName('image', children);
+
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.title}>
-        <span className={styles.innerTitle}>{title}</span>
-      </h1>
-      <div className={styles.content}>{content}</div>
+      <div className={styles.content}>
+        <div className={styles.titleLogo}>
+          <h1 className={styles.title}>{title}</h1>
+          <Link
+            to={sponsorUrl}
+            className={styles.sponsorLogo}
+          >
+            <span className={styles.sponsorName}>{sponsorName}</span>
+            {sponsorLogo}
+          </Link>
+        </div>
+        <div className={styles.description}>{content}</div>
+      </div>
+
     </div>
   );
 };
 
 HubContent.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  sponsorName: PropTypes.string.isRequired,
+  sponsorUrl: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
 };
 
