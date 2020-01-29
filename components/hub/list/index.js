d715535fdd2b50b11e3d894743605d3bcfc4ba13
Gabriel Luethje <gabriel@alley.co>
2020-01-29 11:27:07 -0700

    Clean up list component

--- components/hub/list/index.js
+++ components/hub/list/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'critical-style-loader/lib';
-// import { findChildByName } from 'utils/children';
-// import Link from 'components/helpers/link';
 // Styles
 import styles from './hubList.css';
 
 const HubList = ({
   children,
-}) => {
-  console.log('fuu eslint');
-  return (
-    <ul className={styles.wrapper}>
-      {children}
-    </ul>
-  );
-};
+}) => (
+  <ul className={styles.wrapper}>
+    {children}
+  </ul>
+);
 
 HubList.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
