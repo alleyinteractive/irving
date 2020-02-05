4e3cf7317f75591d451d72a9ca6535320acdb799
Gabriel Luethje <gabriel@alley.co>
2020-02-04 21:07:54 -0700

    Styles & markup for featured podcast component

--- components/podcast/archive/contentItem/index.js
+++ components/podcast/archive/contentItem/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'critical-style-loader/lib';
 import { findChildByName } from 'utils/children';
+import Button from 'components/helpers/button';
 import Link from 'components/helpers/link';
 import withThemes from 'components/hoc/withThemes';
 // Styles
@@ -10,6 +11,7 @@ import featuredTheme from './podcastContentItem--featured.css';
 
 const PodcastContentItem = ({
   children,
+  date,
   eyebrow,
   moreButtonText,
   permalink,
@@ -30,20 +32,35 @@ const PodcastContentItem = ({
           <h2 className={theme.title}>
             <Link to={permalink}>{title}</Link>
           </h2>
-          {time && <span className={theme.time}>{time}</span>}
+          {(time || date) && (
+            <span className={theme.timeDate}>
+              {time && <span className={theme.time}>{time} | </span>}
+              {date && <span className={theme.date}>{date}</span>}
+            </span>
+          )}
           <div className={theme.description}>{description}</div>
-          {moreButtonText && <button type="button">{moreButtonText}</button>}
+
         </div>
         <div className={theme.image}>
           <Link to={permalink}>{image}</Link>
         </div>
       </div>
+      {(moreButtonText && 'featured' === themeName) && (
+        <Button
+          buttonStyle="primary"
+          link={permalink}
+          className={theme.moreButton}
+        >
+          {moreButtonText}
+        </Button>
+      )}
     </Wrapper>
   );
 };
 
 PodcastContentItem.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  date: PropTypes.string.isRequired,
   eyebrow: PropTypes.string.isRequired,
   moreButtonText: PropTypes.string.isRequired,
   permalink: PropTypes.string.isRequired,
