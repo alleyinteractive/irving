cce95c2d6f4ba91ced21fa67170671a6e1268564
Meg Parsons <megparsons@Megs-MacBook-Pro.local>
2020-02-06 15:32:19 -0500

    resolve build conflict

--- components/teaserItem/index.js
+++ components/teaserItem/index.js
@@ -35,13 +35,6 @@ const TeaserItem = ({
   const video = findChildByName('video', children);
   const socialSharing = findChildByName('social-sharing', children);
 
-  if (sharingIsVisible) {
-    document.addEventListener('click', handleCloseSharing, { once: true });
-  }
-  function handleCloseSharing() {
-    setSharingIsVisible(false);
-  }
-
   const otherChildren = children.filter(
     ({ props: { componentName } }) => ('image' !== componentName) &&
       ('video' !== componentName) && ('social-sharing' !== componentName)
