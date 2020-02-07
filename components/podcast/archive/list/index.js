531f5ed09a2582137eaad251799a25c2c93c2599
Gabriel Luethje <gabriel@alley.co>
2020-02-07 14:44:22 -0700

    Clean up naming a bit

--- components/podcast/archive/list/index.js
+++ components/podcast/archive/list/index.js
@@ -9,39 +9,35 @@ import PodcastData from './podcastData';
 import styles from './podcastList.css';
 
 const PodcastList = ({ podcastName }) => {
-  const [episodes, setEpisodes] = useState({
+  const [data, setData] = useState({
     episodes: [],
     lastUpdate: [],
     shouldDisplayLoadMore: true,
   });
-  const [userRequest, setUserRequest] = useState({
+
+  const [endpointData, setEndpointData] = useState({
     currentPage: 1,
     endpoint: `?page=1&podcast_name=${podcastName}`,
   });
 
   const loadItems = () => {
-    const { endpoint, currentPage } = userRequest;
+    const { endpoint, currentPage } = endpointData;
 
-    setUserRequest({
+    setEndpointData({
       currentPage: currentPage + 1,
       endpoint: endpoint.replace(/(page)=[^?&]+/, `$1=${currentPage + 1}`),
     });
   };
 
   const appendEpisodes = (newData, shouldDisplayLoadMore) => {
-    // Ensure that there are no duplicate year cards rendered to the episodes list.
-    setEpisodes({
-      episodes: [...episodes.episodes, ...newData].filter((issue, index, self) => ( // eslint-disable-line
-        index === self.findIndex((i) => (
-          i.config.title === issue.config.title
-        ))
-      )),
+    setData({
+      episodes: [...data.episodes, ...newData],
       lastUpdate: newData,
       shouldDisplayLoadMore,
     });
   };
 
-  const Results = withData(`podcasts${userRequest.endpoint}`, {})(
+  const Results = withData(`podcasts${endpointData.endpoint}`, {})(
     PodcastData
   );
 
@@ -49,19 +45,17 @@ const PodcastList = ({ podcastName }) => {
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <ul className={styles.list}>
-          {episodes.episodes.map((issue) => (
-            <li key={issue.config.title} className={styles.item}>
-              {toReactElement(issue)}
-            </li>
+          {data.episodes.map((episode) => (
+            toReactElement(episode, episode.config.title, {})
           ))}
         </ul>
 
         <Results
           setData={appendEpisodes}
-          lastUpdate={episodes.lastUpdate || []}
+          lastUpdate={data.lastUpdate || []}
         />
 
-        {episodes.shouldDisplayLoadMore && (
+        {data.shouldDisplayLoadMore && (
           <button className={styles.button} type="button" onClick={loadItems}>
             {__('Load more past episodes', 'mittr')}
           </button>
