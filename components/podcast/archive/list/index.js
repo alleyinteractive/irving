61bd5a1dd4e7e3781ff8e302ea6f0e6d05ce67da
Gabriel Luethje <gabriel@alley.co>
2020-02-07 13:45:35 -0700

    Set up load more functionality

--- components/podcast/archive/list/index.js
+++ components/podcast/archive/list/index.js
@@ -1,50 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { __ } from '@wordpress/i18n';
 import { withStyles } from 'critical-style-loader/lib';
-// Styles
+import withData from 'components/hoc/withData';
+import toReactElement from 'utils/toReactElement';
+import PodcastData from './podcastData';
+
 import styles from './podcastList.css';
 
-const PodcastList = ({
-  children,
-}) => (
-  <ul className={styles.wrapper}>
-    {children.map((child, idx) => {
-      const index = idx + 1;
+const PodcastList = ({ podcastName }) => {
+  const [episodes, setEpisodes] = useState({
+    episodes: [],
+    lastUpdate: [],
+    shouldDisplayLoadMore: true,
+  });
+  const [userRequest, setUserRequest] = useState({
+    currentPage: 1,
+    endpoint: `?page=1&podcast_name=${podcastName}`,
+  });
 
-      if (0 === idx) {
-        return (
-          <>
-            <hr className="rowDivider rowDivider--top" />
-            {React.cloneElement(child)}
-          </>
-        );
-      }
+  const loadItems = () => {
+    const { endpoint, currentPage } = userRequest;
 
-      if (0 === index % 3) {
-        return (
-          <>
-            {React.cloneElement(child, { className: 'rowEnd--3up' })}
-            <hr className="rowDivider rowDivider--3up" />
-          </>
-        );
-      }
+    setUserRequest({
+      currentPage: currentPage + 1,
+      endpoint: endpoint.replace(/(page)=[^?&]+/, `$1=${currentPage + 1}`),
+    });
+  };
 
-      if (0 === index % 2) {
-        return (
-          <>
-            {React.cloneElement(child, { className: 'rowEnd--2up' })}
-            <hr className="rowDivider rowDivider--2up" />
-          </>
-        );
-      }
+  const appendEpisodes = (newData, shouldDisplayLoadMore) => {
+    // Ensure that there are no duplicate year cards rendered to the episodes list.
+    setEpisodes({
+      episodes: [...episodes.episodes, ...newData].filter((issue, index, self) => ( // eslint-disable-line
+        index === self.findIndex((i) => (
+          i.config.title === issue.config.title
+        ))
+      )),
+      lastUpdate: newData,
+      shouldDisplayLoadMore,
+    });
+  };
 
-      return React.cloneElement(child);
-    })}
-  </ul>
-);
+  const Results = withData(`podcasts${userRequest.endpoint}`, {})(
+    PodcastData
+  );
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <ul className={styles.list}>
+          {episodes.episodes.map((issue) => (
+            <li key={issue.config.title} className={styles.item}>
+              {toReactElement(issue)}
+            </li>
+          ))}
+        </ul>
+
+        <Results
+          setData={appendEpisodes}
+          lastUpdate={episodes.lastUpdate || []}
+        />
+
+        {episodes.shouldDisplayLoadMore && (
+          <button className={styles.button} type="button" onClick={loadItems}>
+            {__('Load more past episodes', 'mittr')}
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
 
 PodcastList.propTypes = {
-  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  podcastName: PropTypes.string.isRequired,
 };
 
 export default withStyles(styles)(PodcastList);
