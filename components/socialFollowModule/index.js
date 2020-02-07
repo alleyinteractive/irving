392803d9b0cbb5cff13a13ac4c2516f3f280a774
Meg Parsons <megparsons@Megs-MacBook-Pro.local>
2020-02-07 18:03:13 -0500

    follow social media module

--- components/socialFollowModule/index.js
+++ components/socialFollowModule/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import withThemes from 'components/hoc/withThemes';
 import { withStyles } from 'critical-style-loader/lib';
 import { __ } from '@wordpress/i18n';
 // Styles
 import styles from './socialFollowModule.css';
+import darkStyles from './darkTitle.css';
+import lightStyles from './lightTitle.css';
 
-const SocialFollowModule = ({ children }) => (
-  <div className={styles.wrapper}>
-    <span className={styles.title}>{__('Follow', 'mittr')}</span>
+const SocialFollowModule = ({ children, theme }) => (
+  <div className={theme.wrapper}>
+    <span className={theme.title}>
+      {__('Follow', 'mittr')}
+    </span>
     <ul className={styles.social}>
       { children }
     </ul>
@@ -16,6 +21,15 @@ const SocialFollowModule = ({ children }) => (
 
 SocialFollowModule.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  theme: PropTypes.string.isRequired,
 };
 
-export default withStyles(styles)(SocialFollowModule);
+const wrapWithStyles = withStyles(styles, darkStyles, lightStyles);
+
+const wrapWithThemes = withThemes('social-follow-module', {
+  default: styles,
+  dark: darkStyles,
+  light: lightStyles,
+});
+
+export default wrapWithStyles(wrapWithThemes(SocialFollowModule));
