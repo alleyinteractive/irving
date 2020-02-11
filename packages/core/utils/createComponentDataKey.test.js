6d5bf910e9656de01d86b4c66136906a7a897a41
cahdeemer <chrstina@alley.co>
2020-02-11 13:11:04 -0500

    remove excess spaces

--- packages/core/utils/createComponentDataKey.test.js
+++ packages/core/utils/createComponentDataKey.test.js
@@ -1,7 +1,7 @@
 import createComponentDataKey from './createComponentDataKey';
 
 describe('createComponentDataKey', () => {
-  it( 'should replace periods', () => {
+  it('should replace periods', () => {
     expect(createComponentDataKey('fo.o')).toEqual('fo%2Eo');
     expect(createComponentDataKey('.foo')).toEqual('%2Efoo');
     expect(createComponentDataKey('foo.')).toEqual('foo%2E');
@@ -9,21 +9,21 @@ describe('createComponentDataKey', () => {
     expect(createComponentDataKey('.')).toEqual('%2E');
   });
 
-  it( 'should return as is', () => {
+  it('should return as is', () => {
     expect(createComponentDataKey('foo')).toEqual('foo');
     expect(createComponentDataKey('f1f2')).toEqual('f1f2');
     expect(createComponentDataKey('f#23')).toEqual('f#23');
     expect(createComponentDataKey('1foo')).toEqual('1foo');
   });
 
-  it( 'should return a string', () => {
+  it('should return a string', () => {
     expect(createComponentDataKey(1234)).toEqual('1234');
     expect(createComponentDataKey(12.34)).toEqual('12%2E34');
     expect(createComponentDataKey(true)).toEqual('true');
     expect(createComponentDataKey(false)).toEqual('false');
   });
 
-  it( 'should return an empty string', () => {
+  it('should return an empty string', () => {
     expect(createComponentDataKey(undefined)).toEqual('');
     expect(createComponentDataKey(null)).toEqual('');
   });
