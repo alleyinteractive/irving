a965cfe45b52d6fcdff24df814fb412ab8e8fc1e
Pattie Reaves <pattie@alleyinteractive.com>
2020-02-14 11:13:16 -0700

    MIT-662: Delete components frm store on loc chng.

--- reducers/zephrRulesReducer.js
+++ reducers/zephrRulesReducer.js
@@ -1,5 +1,7 @@
 import {
   RECEIVE_ZEPHR_UI_COMPONENTS,
+  REQUEST_ZEPHR_UI_COMPONENTS,
+  LOCATION_CHANGE,
 } from 'actions/types';
 import { zephrRules as defaultState } from './defaultState';
 
@@ -7,6 +9,17 @@ export default function zephrRulesReducer(
   state = defaultState, { type, payload }
 ) {
   switch (type) {
+    case LOCATION_CHANGE:
+      return {
+        ...state,
+        components: {},
+        isLoading: false,
+      };
+    case REQUEST_ZEPHR_UI_COMPONENTS:
+      return {
+        ...state,
+        isLoading: true,
+      };
     case RECEIVE_ZEPHR_UI_COMPONENTS:
       return {
         ...state,
