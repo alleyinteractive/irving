eb16c7b5b8a8a7e583342710a0a6cda483b52054
Matt <mattn@alley.co>
2020-02-18 15:48:02 -0500

    Update index.js

--- components/toggleNotice/index.js
+++ components/toggleNotice/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'critical-style-loader/lib';
+import Arrow from 'assets/icons/arrow.svg';
 
 // Styles
 import styles from './toggleNotice.css';
 
 const ToggleNotice = (props) => {
+  const [meterIsVisible, setMeterIsVisible] = useState(true);
   const { children } = props;
   return (
     <div className={styles.wrapper}>
-      ToggleNotice
-      {children}
+      <div
+        aria-expanded={meterIsVisible}
+      >
+        {children}
+        <button
+          aria-haspopup
+          aria-expanded={meterIsVisible}
+          className={styles.button}
+          onClick={() => {
+            setMeterIsVisible(! meterIsVisible);
+          }}
+          type="button"
+        >
+          <Arrow />
+        </button>
+      </div>
     </div>
   );
 };
