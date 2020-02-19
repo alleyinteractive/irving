af2c6d52428a29bd635ff33afd66960e0d91dd22
Renato Alves <renato@alley.co>
2020-02-18 22:18:59 -0300

    Adding Yoast Schema component/integration

--- packages/integrations/index.js
+++ packages/integrations/index.js
@@ -2,10 +2,6 @@ import Disqus from './components/disqus';
 import GoogleAnalytics from './components/googleAnalytics';
 import GoogleTagManager from './components/googleTagManager';
 import Parsely from './components/parsely';
+import yoastSchema from "./components/yoastSchema";
 
-export {
-  Disqus,
-  GoogleAnalytics,
-  GoogleTagManager,
-  Parsely,
-};
+export { Disqus, GoogleAnalytics, GoogleTagManager, Parsely, yoastSchema ,};
