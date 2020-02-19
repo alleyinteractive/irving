786c03d2ccecef3e52d2e26895b5d2b2f8b3e41b
Renato Alves <renato@alley.co>
2020-02-18 22:24:07 -0300

    Minor tweak

--- packages/integrations/index.js
+++ packages/integrations/index.js
@@ -2,6 +2,6 @@ import Disqus from './components/disqus';
 import GoogleAnalytics from './components/googleAnalytics';
 import GoogleTagManager from './components/googleTagManager';
 import Parsely from './components/parsely';
-import yoastSchema from "./components/yoastSchema";
+import YoastSchema from "./components/yoastSchema";
 
-export { Disqus, GoogleAnalytics, GoogleTagManager, Parsely, yoastSchema ,};
+export { Disqus, GoogleAnalytics, GoogleTagManager, Parsely, YoastSchema };
