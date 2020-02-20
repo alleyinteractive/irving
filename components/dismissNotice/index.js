a1ad23fd1484a418a522b7eb06b81b6275c40ba1
Pattie Reaves <pattie@alleyinteractive.com>
2020-02-20 10:39:02 -0700

    MIT-684: Dismis notice through redux saga.

--- components/dismissNotice/index.js
+++ components/dismissNotice/index.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { withStyles } from 'critical-style-loader/lib';
+import { actionDismissNotice } from 'actions';
+import { __ } from '@wordpress/i18n';
 
 // Styles
 import styles from './dismissNotice.css';
 
-const DismissNotice = (props) => {
-  const { children } = props;
-  return (
-    <div className={styles.wrapper}>
-      {children}
-    </div>
-  );
-};
+const DismissNotice = ({ children, isNoticeVisible, dismiss }) => (
+  <>
+    {isNoticeVisible && (
+      <div className={styles.wrapper}>
+        <div className={styles.component}>
+          {children}
+        </div>
+        <button onClick={dismiss} type="button">
+          {__('Dismiss', 'mittr')}
+        </button>
+      </div>
+    )}
+  </>
+);
 
 DismissNotice.propTypes = {
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  isNoticeVisible: PropTypes.bool.isRequired,
+  dismiss: PropTypes.func.isRequired,
 };
 
-export default withStyles(styles)(DismissNotice);
+const mapDispatchToProps = (dispatch) => ({
+  dismiss: () => dispatch(actionDismissNotice()),
+});
+
+const withRedux = connect(
+  (state) => ({
+    isNoticeVisible: state.isNoticeVisible,
+  }),
+  mapDispatchToProps,
+);
+
+export default withRedux(withStyles(styles)(DismissNotice));
 
--- components/toggleNotice/index.js
+++ components/toggleNotice/index.js
@@ -6,9 +6,8 @@ import Arrow from 'assets/icons/arrow.svg';
 // Styles
 import styles from './toggleNotice.css';
 
-const ToggleNotice = (props) => {
+const ToggleNotice = ({ children }) => {
   const [noticeIsVisible, setNoticeIsVisible] = useState(true);
-  const { children } = props;
   return (
     <div className={styles.wrapper}>
       {/* @todo need to revisit if the aria messaging needs to be entirely in
