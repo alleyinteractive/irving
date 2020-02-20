2aacc2cafc7417c8aba3069dffa77b192063cf2e
Tyler Reckart <tyler@alley.co>
2020-02-20 11:43:12 -0500

    MIT-621: start testing registration errors

--- reducers/zephrReducer.test.js
+++ reducers/zephrReducer.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { merge } from 'lodash/fp';
-import { actionReceiveLoginError } from 'actions/zephrActions';
+import {
+  actionReceiveLoginError,
+  actionReceiveRegistrationError,
+} from 'actions/zephrActions';
 import defaultState from './defaultState';
 import reducer from './zephrReducer';
 
@@ -50,7 +53,93 @@ const loginFormDefaultState = {
   error: false,
   errors: [],
   errorCount: null,
-}
+};
+
+const regFormDefaultState = {
+  components: [
+    {
+      key: 'zephr-input-email-address',
+      ref: null,
+      props: {
+        id: 'email-address',
+        className: 'zephr-input-email-address',
+        type: 'email',
+        placeholder: 'Enter your email address',
+        required: false,
+        defaultValue: '',
+        autoComplete: 'username',
+        children: null,
+        invalid: false,
+      },
+    },
+    {
+      key: 'zephr-input-full-name',
+      ref: null,
+      props: {
+        id: 'full-name',
+        className: 'zephr-input-full-name',
+        type: 'text',
+        placeholder: 'Enter your full name',
+        required: true,
+        defaultValue: '',
+        autoComplete: '',
+        children: null,
+        invalid: false,
+      },
+    },
+    {
+      key: 'zephr-input-new-password',
+      ref: null,
+      props: {
+        id: 'new-password',
+        className: 'zephr-input-new-password',
+        type: 'password',
+        placeholder: 'Create a password for your account',
+        required: true,
+        defaultValue: '',
+        autoComplete: 'new-password',
+        children: null,
+        invalid: false,
+      },
+    },
+    {
+      key: null,
+      ref: null,
+      props: {
+        id: 'verify-password',
+        className: 'zephr-input-verify-password',
+        type: 'password',
+        placeholder: 'Confirm your password',
+        required: true,
+        defaultValue: '',
+        autoComplete: 'new-password',
+        children: null,
+        invalid: false,
+      },
+    },
+    {
+      key: null,
+      ref: null,
+      props: {
+        id: 'terms-checkbox',
+        children: null,
+      },
+    },
+    {
+      type: 'input',
+      key: 'zephr-submit-button',
+      ref: null,
+      props: {
+        type: 'submit',
+        value: 'Create this account',
+        children: null,
+      },
+    },
+  ],
+  error: false,
+  errors: [],
+  errorCount: null,
+};
 
 describe('Zephr Reducer', () => {
   describe('Login actions', () => {
@@ -146,4 +235,53 @@ describe('Zephr Reducer', () => {
       });
     });
   });
+
+  describe('Registration actions', () => {
+    it('Should invalidate the email address field when an invalid email is entered', () => {
+      const mockState = merge(defaultState, {
+        forms: {
+          register: regFormDefaultState,
+        },
+      });
+
+      const components = [...regFormDefaultState.components];
+      const position = 0;
+      const input = components[position];
+      const inputWithErrorState = {
+        ...input,
+        props: {
+          ...input.props,
+          invalid: true,
+        },
+      }
+      // Replace the component with the error state.
+      components.splice(position, 1, inputWithErrorState);
+      // Splice the error message into the components array.
+      const error = React.createElement(
+        'span',
+        {
+          id: `email-address-error`,
+          key: `email-address-error-message`,
+          className: 'form-error',
+        },
+        'Oops! Let’s try that again — Please enter a valid email address.',
+      );
+      components.splice(position + 1, 0, error);
+
+      const newState = reducer(mockState, actionReceiveRegistrationError('email-address'));
+      expect(newState).toEqual({
+        ...mockState,
+        forms: {
+          ...mockState.forms,
+          register: {
+            components,
+            error: true,
+            errors: ['email-address'],
+            errorCount: 1,
+            requireCaptcha: false,
+          },
+        },
+      });
+    });
+  })
 });
