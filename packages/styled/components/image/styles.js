2b07a95eb1a63c20810e4320804b86055bb7bbe0
Owen Stowe <ostowe@gmail.com>
2020-02-29 18:38:03 -0500

    Remove some caption styles

--- packages/styled/components/image/styles.js
+++ packages/styled/components/image/styles.js
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components';
-import { rem } from 'utils';
+import { rem } from '../utils';
 
 const getAspectRatioStyles = (props) => {
   const { aspectRatio } = props;
@@ -39,10 +39,7 @@ export const Wrapper = styled.span
 `;
 
 export const Caption = styled.figcaption`
-  caption-side: bottom;
-  display: table-caption;
-  font-size: ${rem(14)};
-  line-height: ${rem(20)};
+  color: black;
 `;
 
 export const Image = styled.img`
@@ -59,28 +56,6 @@ export const Image = styled.img`
 
     return 'filter: blur(0px);';
   }}
-
-  /* Styles for the SVG placeholder image when no image is set. */
-  .placeholderImg & {
-    left: 50%;
-    max-height: 80%;
-    opacity: 0.5;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    width: auto;
-  }
-
-  /* For content item images we want the image */
-  .contentItem & {
-    height: 100%;
-    object-fit: cover;
-    width: 100%;
-  }
-
-  .featuredImage & {
-    max-width: 100%;
-    width: auto;
-  }
 `;
 
 export const Picture = styled.picture`
