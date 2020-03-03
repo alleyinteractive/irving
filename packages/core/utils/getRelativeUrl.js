9622fa5c690aaa097efc4f6b8169389bfc2f3918
Owen Stowe <ostowe@gmail.com>
2020-03-02 21:29:30 -0500

    Remove console log

--- packages/core/utils/getRelativeUrl.js
+++ packages/core/utils/getRelativeUrl.js
@@ -63,7 +63,6 @@ export default function getRelativeUrl(url) {
     // In case an error occurs, assume URL is not relative.
     result = false;
   }
-  // console.log(url, result);
 
   return result;
 }
