d9038823db378645e095b183ca9a5f5cbf4520ff
Owen Stowe <ostowe@gmail.com>
2020-03-09 20:00:11 -0400

    Update and simplify vip-go cache service
    Much of our logic is now redundant based on recent package updates

--- packages/vip-go/services/cacheService.js
+++ packages/vip-go/services/cacheService.js
@@ -1,3 +1,6 @@
+const getVipRedis = require('@automattic/vip-go');
+const getLogService = require('./logService');
+
 /**
  * @typedef {object} CacheService
  * @property {function} get
@@ -11,31 +14,11 @@ const getService = () => {
   // within a browser context, so that webpack can ignore this execution path
   // while compiling.
   if (! process.env.BROWSER) {
-    // Redis env variables have not been configured.
-    if (! process.env.REDIS_MASTER) {
+    const client = getVipRedis({ logger: getLogService('irving:redis') });
+
+    if (! client) {
       return false;
     }
-
-    let Redis;
-    // Check if optional redis client is installed.
-    try {
-      Redis = require('ioredis'); // eslint-disable-line global-require
-    } catch (err) {
-      return false;
-    }
-
-    const [host, port] = (process.env.REDIS_MASTER).split(':');
-    const opts = { host, port };
-
-    // Add password, if configured
-    if (process.env.REDIS_PASSWORD) {
-      opts.password = process.env.REDIS_PASSWORD;
-    }
-
-    const client = new Redis(opts);
-    client.on('error', (err) => {
-      console.error(err); // eslint-disable-line no-console
-    });
 
     return {
       client,
@@ -47,7 +30,7 @@ const getService = () => {
           key,
           JSON.stringify(value),
           'EX',
-          600
+          process.env.CACHE_EXPIRE || 300
         );
       },
       del(key) {
