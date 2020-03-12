d7214ef5a249b054b28cfa2099dcb4181d7ee927
Owen Stowe <ostowe@gmail.com>
2020-03-11 20:10:36 -0400

    Fix syntax error

--- packages/core/config/webpack/rules.js
+++ packages/core/config/webpack/rules.js
@@ -118,7 +118,7 @@ module.exports = function getRules(context) {
                 filepath.includes('split-on-first') ||
                 filepath.includes('strict-uri-encode') ||
                 filepath.includes('abort-controller') ||
-                filepath.includes('event-target-shim') ||
+                filepath.includes('event-target-shim')
               )
           ),
         ],
