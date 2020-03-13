22d7e0b0ac240b3f2aff397fd5eb6b3a34c099c9
Zack Rothauser <zack@alley.co>
2020-03-13 10:46:23 -0700

    Styling and markup for 50 list header

--- config/componentMap.js
+++ config/componentMap.js
@@ -43,6 +43,7 @@ import LinkTeaser from 'components/linkTeaser';
 import ListDescription from 'components/listDescription';
 import ListGroup from 'components/listGroup';
 import ListHeader from 'components/listHeader';
+import List50Header from 'components/list50Header';
 import ListItem from 'components/listItem';
 import ListMenu from 'components/listMenu';
 import ListMenuItem from 'components/listMenuItem';
@@ -150,6 +151,7 @@ export const componentMap = {
   'link-teaser': LinkTeaser,
   'innovator-header': InnovatorHeader,
   'list-header': ListHeader,
+  'list-50-header': List50Header,
   'list-description': ListDescription,
   'list-menu': ListMenu,
   'list-menu-item': ListMenuItem,
