0af3fff2cf807ab64025963b7349713c11a8f003
Gabriel Luethje <gabriel@alley.co>
2020-03-19 15:25:21 -0700

    Add companyFlyout component to companyListItemRank component

--- components/companyListItemRank/index.js
+++ components/companyListItemRank/index.js
@@ -2,23 +2,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'critical-style-loader/lib';
+import { findChildByName } from 'utils/children';
 import styles from './companyListItemRank.css';
 
 const CompanyListItemRank = (props) => {
   const {
+    children,
     companyName,
     rank,
   } = props;
 
+  const flyout = findChildByName('list-50-item-flyout', children);
   return (
     <li className={styles.wrapper}>
       <span className={styles.rankWrapper}>{rank}</span>
       <span className={styles.nameWrapper}>{companyName}</span>
+      {flyout}
     </li>
   );
 };
 
 CompanyListItemRank.propTypes = {
+  children: PropTypes.node.isRequired,
   companyName: PropTypes.string.isRequired,
   rank: PropTypes.number.isRequired,
 };
