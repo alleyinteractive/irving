41dc9cb1380c3adb5818eae1647d3f64bedc6991
cahdeemer <chrstina@alley.co>
2020-03-24 13:54:26 -0400

    rename some things

--- packages/core/reducers/errorReducer.test.js
+++ packages/core/reducers/errorReducer.test.js
@@ -1,41 +1,41 @@
 import errorReducer from './errorReducer';
 import { LOCATION_CHANGE, RECEIVE_ERROR } from 'actions/types';
-import { error as defaultState } from 'reducers/defaultState';
+import { error as errorState } from 'reducers/defaultState';
 
 describe('errorReducer', () => {
-  it('Should return the default state when the location changes', () => {
+  it('Should return the errorState state when the location changes', () => {
     const action = {
       type: LOCATION_CHANGE,
       payload: {
         endpoint: 'testEndpoint',
       },
     };
-    expect(errorReducer(defaultState, action)).toEqual(defaultState);
+    expect(errorReducer(errorState, action)).toEqual(errorState);
   });
 
   it('Should return the payload when the app receives an unrecoverable error', () => {
     const action = {
       type: RECEIVE_ERROR,
       payload: {
-        endpoint: 'testEndpoint',
+        endpoint: 'test error endpoint',
       },
     };
-    expect(errorReducer(defaultState, action)).toEqual(action.payload);
+    expect(errorReducer(errorState, action)).toEqual(action.payload);
   });
 
-  it('Should return the default state when there is no type', () => {
+  it('Should return errorState when there is no type', () => {
     const action = {
       payload: {
         endpoint: 'testEndpoint',
       },
     };
-    expect(errorReducer(defaultState, action)).toEqual(defaultState);
+    expect(errorReducer(errorState, action)).toEqual(errorState);
   });
 
-  it('Should return the default state when there is no payload', () => {
+  it('Should return errorState when there is no payload', () => {
     const action = {
       type: LOCATION_CHANGE,
     };
-    expect(errorReducer(defaultState, action)).toEqual(defaultState);
+    expect(errorReducer(errorState, action)).toEqual(errorState);
   });
 });
