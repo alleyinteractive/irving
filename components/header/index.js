a6713c4e628760878c834794843764decea8e0ad
Gabriel Luethje <gabriel@alley.co>
2020-03-24 15:11:35 -0700

    Add nprogress to header

--- components/header/index.js
+++ components/header/index.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Headroom from 'react-headroom';
+import NProgress from 'nprogress';
 import { withStyles } from 'critical-style-loader/lib';
 import useBreakpoint from 'hooks/useBreakpoint';
 import HeaderTemplate from 'components/headerTemplate';
 
 // Styles
 import styles from './header.css';
-
+import './nprogress.css';
 const Header = (props) => {
   const { homeUrl, children } = props;
   const [isMobile, setIsMobile] = useState(false);
   // Breakpoints
   const isSmMin = useBreakpoint('smMin');
+
   // When resizing, do not have the click to expand controls on desktop.
   useEffect(() => {
     if (! isSmMin) {
@@ -22,6 +24,12 @@ const Header = (props) => {
     }
   });
 
+  useEffect(() => {
+    NProgress.configure({ parent: '#siteHeader' });
+    NProgress.set(0.4);
+  }, []);
+
+  console.log(NProgress);
   return (
     <>
       <HeaderTemplate
@@ -40,6 +48,7 @@ const Header = (props) => {
       >
         <HeaderTemplate
           isHeadroom
+          id="siteHeader"
           homeUrl={homeUrl}
           isMobile={isMobile}
         >
