0fec7ac5adc284fce286183f97cc716be32ad003
Eric Nation <eric.nation@gmail.com>
2020-04-01 10:12:32 -0600

    MIT-889: convert inline style to class name

--- assets/icons/monogramT-logo.js
+++ assets/icons/monogramT-logo.js
@@ -1,7 +1,7 @@
 const monogramTLogo = `<svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 288 287.99"
-  style="margin-left: 10px; width: 14px; height: 14px;">
+  class="monogramTLogo">
     <path
       d="M199.49,66.67,133,133.11H66.56V66.67Zm44.31,0-66.46,66.44V354.66h66.46V133.11H354.56V66.67Z"
       transform="translate(-66.56 -66.67)"></path>
