3561a34d7c36457ef70f03376d69003eec7df6dd
Pattie Reaves <pattie@alleyinteractive.com>
2020-04-05 21:29:58 -0700

    Fix console error when request not found.

--- services/zephrUIService.js
+++ services/zephrUIService.js
@@ -35,7 +35,7 @@ export async function fetchZephrUIComponents({ pageID, session }) {
   }
 
   if (! response.ok) {
-    debug(response.text());
+    debug(await response.text());
     throw new Error(await response.text());
   }
 
