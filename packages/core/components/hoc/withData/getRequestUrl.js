a02231e08e400aa7a0aca2e856156d757036e18c
Renato Alves <renato@alley.co>
2020-04-10 23:30:53 -0300

    Final tweaks

--- packages/core/components/hoc/withData/getRequestUrl.js
+++ packages/core/components/hoc/withData/getRequestUrl.js
@@ -1,9 +1,14 @@
 const getRequestUrl = (endpoint) => {
   switch (true) {
+    // If endpoint is absolute, use it as-is.
     case endpoint.includes('://'):
       return endpoint;
+
+    // Endpoint is relative, add it to end of configured API_ROOT_URL
     case endpoint.includes('/'):
       return `${process.env.API_ROOT_URL}/${endpoint}`;
+
+    // Or use component data endpoint.
     default:
       return `${process.env.API_ROOT_URL}/data/${endpoint}`;
   }
