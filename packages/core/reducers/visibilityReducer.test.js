bf75bb82ef9e4bdae11042345cd0facd4aae2e9e
Renato Alves <renato@alley.co>
2020-04-10 23:53:05 -0300

    Cleaning up and adding describe to a few tests

--- packages/core/reducers/visibilityReducer.test.js
+++ packages/core/reducers/visibilityReducer.test.js
@@ -2,20 +2,22 @@ import { actionUpdateVisibility } from 'actions';
 import { visible as defaultState } from './defaultState';
 import reducer from './visibilityReducer';
 
-it('should set field visible', () => {
-  const mockState = { ...defaultState, foo: false };
-  const newState = reducer(mockState, actionUpdateVisibility('foo', true));
-  expect(newState.foo).toBe(true);
-});
+describe('reducer', () => {
+  it('should set field visible', () => {
+    const mockState = { ...defaultState, foo: false };
+    const newState = reducer(mockState, actionUpdateVisibility('foo', true));
+    expect(newState.foo).toBe(true);
+  });
 
-it('should set field not visible', () => {
-  const mockState = { ...defaultState, foo: true };
-  const newState = reducer(mockState, actionUpdateVisibility('foo', false));
-  expect(newState.foo).toBe(false);
-});
+  it('should set field not visible', () => {
+    const mockState = { ...defaultState, foo: true };
+    const newState = reducer(mockState, actionUpdateVisibility('foo', false));
+    expect(newState.foo).toBe(false);
+  });
 
-it('should toggle field visibility', () => {
-  const mockState = { ...defaultState, foo: false };
-  const newState = reducer(mockState, actionUpdateVisibility('foo'));
-  expect(newState.foo).toBe(true);
+  it('should toggle field visibility', () => {
+    const mockState = { ...defaultState, foo: false };
+    const newState = reducer(mockState, actionUpdateVisibility('foo'));
+    expect(newState.foo).toBe(true);
+  });
 });
