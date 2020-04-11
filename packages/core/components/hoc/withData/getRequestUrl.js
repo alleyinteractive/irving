915324d6f8606d62ccfdb37533766060b853c7a5
Renato Alves <renato@alley.co>
2020-04-10 23:24:32 -0300

    Refactoring and adding tests for the getRequestUrl function

--- packages/core/components/hoc/withData/getRequestUrl.js
+++ packages/core/components/hoc/withData/getRequestUrl.js
@@ -1,26 +1,12 @@
-import URL from 'url-parse';
-
 const getRequestUrl = (endpoint) => {
-  let requestUrl;
-
-  if (endpoint.includes('/')) {
-    try {
-      // If endgpoint is absolute, use it as-is.
-      const urlObj = new URL(endpoint);
-
-      if (urlObj.host) {
-        requestUrl = endpoint;
-      }
-    } catch (e) {
-      // Endpoint is relative, add it to end of configured API_ROOT_URL
-      requestUrl = `${process.env.API_ROOT_URL}/${endpoint}`;
-    }
-  } else {
-    // Use component data endpoint.
-    requestUrl = `${process.env.API_ROOT_URL}/data/${endpoint}`;
+  switch (true) {
+    case endpoint.includes('://'):
+      return endpoint;
+    case endpoint.includes('/'):
+      return `${process.env.API_ROOT_URL}/${endpoint}`;
+    default:
+      return `${process.env.API_ROOT_URL}/data/${endpoint}`;
   }
-
-  return requestUrl;
 };
 
 export default getRequestUrl;
