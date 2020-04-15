99bcb2f0628733812ad421d5682b28651bab110b
Owen Stowe <ostowe@gmail.com>
2020-04-15 19:43:03 -0400

    Add logic for making authorized components endpoint requests

---
+++ packages/core/sagas/resolveComponentsAuthorized.js
@@ -0,0 +1,31 @@
+import {
+  call,
+  put,
+  select,
+} from 'redux-saga/effects';
+import {
+  actionReceiveComponents,
+  actionReceiveError,
+} from 'actions';
+import getRouteMeta from 'selectors/getRouteMeta';
+import { fetchComponents } from 'services/fetchComponents';
+import createDebug from 'services/createDebug';
+
+const debug = createDebug('sagas:components:authorized');
+
+export default function* resolveComponentsAuthorized() {
+  const {
+    path,
+    search,
+    cookie,
+    context,
+  } = yield select(getRouteMeta);
+
+  try {
+    const result = yield call(fetchComponents, path, search, cookie, context);
+    yield put(actionReceiveComponents(result));
+  } catch (err) {
+    yield call(debug, err);
+    yield put(actionReceiveError(err));
+  }
+}
