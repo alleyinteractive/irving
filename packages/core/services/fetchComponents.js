d8afee3a1ebfb80fcfe79ab9c36ea03bb94429dd
Owen Stowe <ostowe@gmail.com>
2020-04-21 10:13:39 -0400

    Update packages/core/services/fetchComponents.js
    Co-Authored-By: James Burke <james.walter.burke@gmail.com>

--- packages/core/services/fetchComponents.js
+++ packages/core/services/fetchComponents.js
@@ -55,6 +55,7 @@ export async function fetchComponents(
   // Set up Authorization header, if applicable.
   const authorizationBearerToken = getBearerToken(cookie);
   if (authorizationBearerToken) {
+    options.credentials = 'same-origin'; // Set to same origin so we don't conflict with other cookies.
     options.headers.Authorization = `Bearer ${authorizationBearerToken}`;
   }
 
