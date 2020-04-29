0e1d335259aa8682d1079b34257275c37673a69e
Owen Stowe <ostowe@gmail.com>
2020-04-29 12:49:48 -0400

    fix(withthemes): add back in a components/withThemes export to prevent breakage

---
+++ packages/styled/components/withThemes.js
@@ -0,0 +1,8 @@
+import withThemes from './hoc/withThemes';
+
+/**
+ * This exists just to prevent projects with existing imports for this file from breaking.
+ *
+ * @todo remove this file at next major release.
+ */
+export default withThemes;
