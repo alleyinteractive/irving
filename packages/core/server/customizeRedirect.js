298383ec6fc08653d34e14cd79f4e45c013e8a27
Renato Alves <renato@alley.co>
2020-05-18 15:12:56 -0300

    refactor: refactoring to account for the ROOT_URL in redirects

--- packages/core/server/customizeRedirect.js
+++ packages/core/server/customizeRedirect.js
@@ -1,15 +1,23 @@
 const expressNakedRedirect = require('express-naked-redirect');
 const getConfigField = require('../utils/getConfigField');
+const {
+  ROOT_URL,
+} = process.env;
 
 module.exports = function () {
   const config = getConfigField('customizeRedirect');
 
-  // Move on if not config is added.
-  if (Object.keys(config).length === 0) {
-    return function (req, res, next) {
+  return function (req, res, next) {
+    // Move on if no config was added.
+    if (Object.keys(config).length === 0) {
       return next();
-    };
-  }
+    }
 
-  return expressNakedRedirect(config);
+    // Confirm if request with the root url.
+    if (!ROOT_URL.includes(req.headers.host)) {
+      return next();
+    } else {
+      return expressNakedRedirect(config)(req, res, next);
+    }
+  };
 };
