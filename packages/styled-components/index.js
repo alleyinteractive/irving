cc72959e1496fd6766ad422454f980c192c8f614
James Burke <james.walter.burke@gmail.com>
2020-06-10 17:19:28 -0400

    Update export to have component map

--- packages/styled-components/index.js
+++ packages/styled-components/index.js
@@ -6,14 +6,20 @@ import HTML from 'components/html';
 import Link from 'components/link';
 import Logo from 'components/logo';
 import Menu from 'components/menu';
+import { Helmet } from 'react-helmet';
 
 export {
-  App,
-  Byline,
-  Container,
-  Fragment,
-  HTML,
-  Link,
-  Logo,
-  Menu,
-};
+  '': Fragment,
+  'irving/body-wrapper': Fragment,
+  'irving/container': Container,
+  'irving/footer-wrapper': Fragment,
+  'irving/fragment': Fragment,
+  'irving/header-wrapper': Fragment,
+  'irving/helmet': Helmet,
+  'irving/html': HTML,
+  'irving/link': Link,
+  'irving/logo': Logo,
+  'irving/menu': Menu,
+  'irving/post-byline': Byline,
+  app: App,
+}
