25e59ef220f4fe953a666eb3845c3c26c925dd59
James Burke <james.walter.burke@gmail.com>
2020-06-16 16:44:33 -0400

    Implement styled components storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
     '../stories/**/*.stories.js',
-    '../packages/core/components/**/*.stories.(js|mdx)',
+    '../packages/styled-components/components/**/*.stories.(js|mdx)',
   ],
   addons: [
     '@storybook/addon-actions',
