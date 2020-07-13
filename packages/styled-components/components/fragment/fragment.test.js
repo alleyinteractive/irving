af25dac4bdc8d3eaf984308b73acd4256784a8eb
cahdeemer <chrstina@alley.co>
2020-07-13 12:22:43 -0400

    test(fragment component): refactor fragment component to use RTL

--- packages/styled-components/components/fragment/fragment.test.js
+++ packages/styled-components/components/fragment/fragment.test.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Fragment from './';
 
 describe('<Fragment />', () => {
   it('renders children components', () => {
-    const wrapper = mount(<Fragment children={['Foo Bar']} />);
-    expect(wrapper.text()).toBe('Foo Bar');
+    render(<Fragment children={['Foo Bar']} />);
+    expect(screen.getByText('Foo Bar')).toBeInTheDocument();
   });
 
   it('renders the correct tag', () => {
-    const wrapper = shallow(<Fragment tag="section" children={['Foo Bar']} />);
-    expect(wrapper.render()[0].name).toBe('section');
+    const { container } = render(<Fragment tag="h1" children={['Foo Bar']} />);
+    expect(container.querySelector('h1')).toBeInTheDocument();
+  });
+
+  it('renders the correct role', () => {
+    render(<Fragment tag="h3" children={['Foo Bar']} />);
+    expect(screen.getByRole('heading', { name: 'Foo Bar'})).toBeInTheDocument();
   });
 });
