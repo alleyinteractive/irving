255720f5f69f6b19046aef3da89df4e2e903d60c
Owen Stowe <ostowe@gmail.com>
2020-07-31 15:13:31 -0400

    fix(usebreakpoint): remove reference to config/breakpoints from useBreakpoint, rename it

--- packages/core/hooks/useBreakpoint.js
+++ packages/core/hooks/useBreakpoint.js
@@ -1,30 +1,25 @@
 import { useEffect, useState } from 'react';
-import breakpoints from 'config/breakpoints';
 
 /**
- * Hook for creating an instance of matchMedia for checking a particular breakpoint.
+ * Hook for creating an instance of matchMedia for checking a particular breakpoint or media query.
  *
- * @param {string} breakpointName Breakpoint name. Must correspond to one of the keys in config/css/breakpoints.js
- * @returns {bool} Does the viewport width match this breakpoint/media query?
+ * @param {string} mediaQuery Media query to check against
+ * @returns {bool} Does the viewport width match this breakpoint or media query?
  */
-export default function useBreakpoint(breakpointName) {
-  if (breakpoints[breakpointName]) {
-    const mq = window.matchMedia(`(${breakpoints[breakpointName]})`);
-    const [matches, setMatches] = useState(mq.matches);
-    const checkMq = (e) => {
-      setMatches(e.matches);
+export default function useMedia(mediaQuery) {
+  const mq = window.matchMedia(mediaQuery);
+  const [matches, setMatches] = useState(mq.matches);
+  const checkMq = (e) => {
+    setMatches(e.matches);
+  };
+
+  useEffect(() => {
+    mq.addListener(checkMq);
+
+    return function cleanup() {
+      mq.removeListener(checkMq);
     };
+  });
 
-    useEffect(() => {
-      mq.addListener(checkMq);
-
-      return function cleanup() {
-        mq.removeListener(checkMq);
-      };
-    });
-
-    return matches;
-  }
-
-  return false;
+  return matches;
 }
