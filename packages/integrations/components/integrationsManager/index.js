cce865765e5ccf61b953473d2161d697acc3020f
Tyler Reckart <tyler@alley.co>
2020-08-03 14:56:00 -0400

    IRV-592: make integration consumption dynamic

--- packages/integrations/components/integrationsManager/index.js
+++ packages/integrations/components/integrationsManager/index.js
@@ -1,30 +1,70 @@
-/* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-// Integration Components.
+// Integrations.
 import GoogleAnalytics from '../googleAnalytics';
 
 const IntegrationsManager = ({ integrations }) => {
-  const [componentMap, setComponentMap] = useState([]);
+  // Build an initial component map of available integration components.
+  // The components will not be rendered until the `active` property has been
+  // set to true. This is done dynamically through the `useEffect` method
+  // below.
+  const [componentMap, setComponentMap] = useState([
+    {
+      key: 'googleAnalytics',
+      el: <GoogleAnalytics trackingId="" />,
+      active: false,
+    },
+  ]);
 
+  // This effect is run in order to populate the component map as props are
+  // updated and recieved. It will only be run on initial mount and if/when the
+  // whitelisted props change during the component lifecycle.
   useEffect(() => {
-    const componentKeys = componentMap.map((i) => i.key);
+    // Retrieve the keys available for render in the component map.
+    const keyMap = Object.keys(integrations);
 
-    if (integrations.googleAnalytics !== undefined && componentKeys.indexOf('googleAnalytics') === -1) {
-      const { trackingId } = integrations.googleAnalytics;
+    componentMap.forEach((component, index) => {
+      // Get the component's key.
+      const { key } = component;
 
-      setComponentMap([
-        ...componentMap,
-      { key: 'googleAnalytics', el: <GoogleAnalytics trackingId={trackingId} /> },
-      ]);
-    }
+      if (- 1 < keyMap.indexOf(key)) {
+        // Retrieve the key/value pairs set for the integration and convert them into props.
+        const props = integrations[key];
+        // Clone the component with the associated props and a key for render mapping.
+        const componentWithProps = React.cloneElement(
+          component.el,
+          { ...props, key },
+          null
+        );
+        // Clone the component map.
+        const components = [...componentMap];
+        // Update the item at the current index with the transformed component.
+        components[index] = { key, el: componentWithProps, active: true };
+        // Update the component map.
+        setComponentMap(components);
+      }
+    });
   }, [integrations]);
 
   return (
     <>
-      {componentMap.map((comp) => comp.el)}
+      {componentMap.map(
+        (component) => component.active && component.el
+      )}
     </>
   );
+};
+
+IntegrationsManager.defaultProps = {
+  integrations: {},
+};
+
+IntegrationsManager.propTypes = {
+  integrations: PropTypes.shape({
+    googleAnalytics: PropTypes.shape({
+      trackingId: PropTypes.string,
+    }),
+  }),
 };
 
 export default IntegrationsManager;
