08217c4620734b36acdaa506506f51c58393c8a8
Tyler Reckart <tyler@alley.co>
2020-08-11 15:12:48 -0400

    IRV-615: add yoast schmea to manager map

--- packages/integrations/components/integrationsManager/componentMap.js
+++ packages/integrations/components/integrationsManager/componentMap.js
@@ -1,9 +1,11 @@
 // Integration components.
 import GoogleAnalytics from '../googleAnalytics';
 import CoralEmbed from '../coral';
+import YoastSchema from '../yoastSchema';
 
 // Declare a map of available integration components with a corresponding key.
 export default [
   { key: 'googleAnalytics', el: GoogleAnalytics },
   { key: 'coral', el: CoralEmbed },
+  { key: 'yoastSchema', el: YoastSchema },
 ];
