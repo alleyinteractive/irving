a8e3bd7f67c2c5d29d78d73b597ca158ab1c798a
Tyler Reckart <tyler@alley.co>
2020-08-13 10:04:06 -0400

    IRV-614: inject tracking gif into dom

--- packages/integrations/components/integrationsManager/componentMap.js
+++ packages/integrations/components/integrationsManager/componentMap.js
@@ -2,10 +2,12 @@
 import GoogleAnalytics from '../googleAnalytics';
 import CoralEmbed from '../coral';
 import YoastSchema from '../yoastSchema';
+import SiteStats from '../siteStats';
 
 // Declare a map of available integration components with a corresponding key.
 export default [
   { key: 'googleAnalytics', el: GoogleAnalytics },
   { key: 'coral', el: CoralEmbed },
   { key: 'yoastSchema', el: YoastSchema },
+  { key: 'siteStats', el: SiteStats },
 ];
