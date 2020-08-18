493698785567a3e43e5e70c0adfb0d67769eecb9
Tyler Reckart <tyler@alley.co>
2020-08-18 14:18:58 -0400

    DEF-56: refine observer

--- packages/integrations/components/coral/withPico.js
+++ packages/integrations/components/coral/withPico.js
@@ -5,42 +5,57 @@ const withPico = (ChildComponent) => (props) => {
   const [picoLoaded, setPicoLoaded] = useState(false);
 
   useEffect(() => {
-    const initHandler = () => {
+    // The observer variable needs to be defined outside of the init context
+    // so that it can be disconnected when the `withPico` component unmounts.
+    let observer;
+
+    const loadHandler = () => {
       setPicoLoaded(true);
     };
-    document.addEventListener('pico-init', initHandler);
 
-    const picoButton = document.getElementById('PicoSignal-button');
-    if (picoButton) {
-      const observer = new MutationObserver((mutations) => {
-        console.log('mutations => ', mutations);
+    // Listen for pico to be initialized.
+    document.addEventListener('pico-init', loadHandler);
 
-        mutations.forEach((mutation) => {
-          const {
-            type,
-            attributeName,
-          } = mutation;
+    if (picoLoaded) {
+      const picoButton = document.getElementById('PicoSignal-button');
+      // Only mount the observer if the PicoSignal button exists in the DOM and
+      // the Pico data attributes have been appended to the element.
+      if (picoButton && null !== picoButton.getAttribute('data-pico-email')) {
+        // Define the observer.
+        observer = new MutationObserver((mutations) => {
+          mutations.forEach((mutation) => {
+            const {
+              type,
+              attributeName,
+            } = mutation;
+            // Execute the webhook on the `data-pico-status` attribute.
+            if ('attributes' === type && 'data-pico-status' === attributeName) {
+              // Create a reusable store for attribute values.
+              const attributes = {
+                registered: picoButton.getAttribute('data-pico-status'),
+                email: picoButton.getAttribute('data-pico-email'),
+              };
 
-          if ('attributes' === type && 'data-pico-status' === attributeName) {
-            const attributes = {
-              registered: picoButton.getAttribute('data-pico-status'),
-              email: picoButton.getAttribute('data-pico-email'),
-            };
-            const isRegistered = 'registered' === attributes.registered;
-            const hasEmail = 0 < attributes.email.length;
+              const isRegistered = 'registered' === attributes.registered;
+              const hasEmail = 0 < attributes.email.length;
 
-            if (isRegistered && hasEmail) {
-              // do something.
-              console.log('userEmail => ', attributes.email);
+              if (isRegistered && hasEmail) {
+                // do something.
+                console.log(attributes);
+              }
             }
-          }
+          });
         });
-      });
-      observer.observe(picoButton, { attributes: true });
+        observer.observe(picoButton, { attributes: true });
+      }
     }
 
     return () => {
-      document.removeEventListener('pico-init', initHandler);
+      document.removeEventListener('pico-init', loadHandler);
+
+      if (observer) {
+        observer.disconnect();
+      }
     };
   }, [picoLoaded]);
 
