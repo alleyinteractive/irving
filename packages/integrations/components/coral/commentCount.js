a2015d5cd2cab6ea10bd8dda56e1dc50fb6ea864
Joe McGill <joemcgill@gmail.com>
2020-08-24 15:19:08 -0500

    Add Coral Comment Count component
    Adds:
    * CommentCount component inside the Coral integrations folder for displaying comment counts of an article from Coral.

---
+++ packages/integrations/components/coral/commentCount.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import useLoadScript from '@irvingjs/core/hooks/useLoadScript';
+
+const CommentCount = (props) => {
+  const {
+    articleUrl,
+    embedUrl,
+    noText,
+  } = props;
+
+  if (! embedUrl) {
+    return null;
+  }
+
+  const loaded = useLoadScript(
+    `${embedUrl}/assets/js/count.js`,
+    null,
+    {
+      attr: {
+        className: 'coral-script',
+        defer: true,
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (window.Coral) {
+      console.log('Coral Comment Count');
+    }
+  }, [loaded]);
+
+  return (
+    <span
+      className="coral-count"
+      data-coral-url={articleUrl}
+      data-coral-notext={noText}
+    />
+  );
+};
+
+CommentCount.defaultProps = {
+  noText: false,
+};
+
+CommentCount.propTypes = {
+  articleUrl: PropTypes.string.isRequired,
+  embedUrl: PropTypes.string.isRequired,
+  noText: PropTypes.bool,
+};
+
+export default CommentCount;
