2926244e405c209963dc2f0c62e4baa1e9729d3b
Owen Stowe <ostowe@gmail.com>
2020-09-04 15:35:36 -0400

    fix(app): separate connectedroot from app wrapper compopnent
    Also add in `time` and `h1` to html sanitization allowedTags

---
+++ packages/core/components/root/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import RootProviders from 'components/rootProviders';
+import ConnectedRoot from 'components/connectedRoot';
+import getRoots from 'selectors/getRoots';
+import getProviders from 'selectors/getProviders';
+
+const Root = (props) => {
+  const {
+    roots,
+    providers,
+  } = props;
+
+  return (
+    <RootProviders providers={providers}>
+      {roots.map((name) => (
+        <ConnectedRoot key={name} name={name} />
+      ))}
+    </RootProviders>
+  );
+};
+
+Root.propTypes = {
+  /**
+   * Root component configurations
+   */
+  roots: PropTypes.arrayOf(PropTypes.string).isRequired,
+  /**
+   * Root provider configurations
+   */
+  providers: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  roots: getRoots(state),
+  providers: getProviders(state),
+  error: !! state.error,
+});
+
+export default connect(mapStateToProps)(Root);
--- packages/core/config/html.js
+++ packages/core/config/html.js
@@ -1,9 +1,9 @@
 export const richText = {
   allowedTags: [
-    'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
     'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'cite', 'hr', 'br',
     'div', 'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img',
-    'figure', 'figcaption', 'iframe', 'audio', 'video',
+    'figure', 'figcaption', 'iframe', 'audio', 'video', 'time',
   ],
   allowedAttributes: {
     '*': ['class', 'id', 'data-*', 'style'],
