96d8ff1c8274a9a954013cdce92ae84ea3c1d1eb
Tyler Reckart <tyler@alley.co>
2020-09-10 16:29:21 -0400

    Explicitly render embed based on tier

--- packages/integrations/components/coral/withPico.js
+++ packages/integrations/components/coral/withPico.js
@@ -1,8 +1,12 @@
-import React, { useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import CoralEmbed from './index';
 import { actionRequireUpgrade } from '../../actions/picoActions';
-import { tokenSelector } from '../../selectors/coralSelector';
+import {
+  tokenSelector,
+  verificationRequestSelector,
+} from '../../selectors/coralSelector';
+import { picoLoadedSelector } from '../../selectors/picoSelector';
 
 const withPico = (ChildComponent) => (props) => {
   const dispatch = useDispatch();
@@ -17,6 +21,7 @@ const withPico = (ChildComponent) => (props) => {
   // Define Coral event handlers.
   const handlers = (events) => {
     events.on('loginPrompt', () => {
+      console.log('bump');
       const signalNode = document.getElementById('PicoSignal-container');
 
       if (signalNode) {
@@ -51,16 +56,53 @@ const withPico = (ChildComponent) => (props) => {
     });
   };
 
+  const picoLoaded = useSelector(picoLoadedSelector);
+  const requestSent = useSelector(verificationRequestSelector);
+  const [canComment, setCanComment] = useState(null);
+
+  useEffect(() => {
+    const signalNode = document.getElementById('PicoSignal-container');
+
+    if (signalNode) {
+      const tier = signalNode.getAttribute('data-pico-tier');
+
+      if (! tier && null !== tier) {
+        setCanComment(false);
+      }
+
+      if (tiers.includes(tier)) {
+        setCanComment(true);
+      }
+
+      if (! tiers.includes(tier) && null !== tier) {
+        setCanComment(false);
+      }
+    }
+  }, [picoLoaded, requestSent]);
+
   // Retrieve the Coral SSO token from the Redux store.
   const coralToken = useSelector(tokenSelector);
 
-  return (
-    <ChildComponent
-      {...props}
-      events={handlers}
-      accessToken={coralToken}
-    />
-  );
+  if (coralToken && canComment && requestSent) {
+    return (
+      <ChildComponent
+        {...props}
+        events={handlers}
+        accessToken={coralToken}
+      />
+    );
+  }
+
+  if (false === canComment) {
+    return (
+      <ChildComponent
+        {...props}
+        events={handlers}
+      />
+    );
+  }
+
+  return null;
 };
 
 export default withPico(CoralEmbed);
