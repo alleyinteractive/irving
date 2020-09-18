alleyinteractive/irving

commit b59076722bcf935e1b262ce93426127a534af1b3

fix(pico): Trigger Pico actions on pre-fetched pages
This fixes a bug where pre-fetched pages were not triggering Pico actions
because the Pico component was being rendered from the previous page's
state. We had previously included a check for whether we had already
dispatched a Pico page update using a `useState` hook to bypass duplicate
dispatches and that state was not being reset between pageviews if the Pico
component instance was being reused.

This refactors the logic for side effects into several separate `useEffect`
hooks to separate responsibilities for each side effect and simplify the
number of dependencies each `useEffect` checks before running.

IRV-710

packages/integrations/components/pico/index.js
@@ -43,15 +43,9 @@ const Pico = (props) => {
   const [picoInitialized, setPicoInitialized] = useState(false);
   // Create the dispatch function.
   const dispatch = useDispatch();
-  // Create a state value that prevents multiple `UPDATE_PICO_PAGE_INFO` actions
-  // from being fired on a single `LOCATION_CHANGE`.
-  const [picoPageInfoUpdated, setPicoPageInfoUpdated] = useState(false);
   // Create a function that dispatches the current page info for the saga to respond to.
   const dispatchUpdatePicoPageInfo = useCallback(
-    (payload) => {
-      setPicoPageInfoUpdated(true);
-      return dispatch(actionUpdatePicoPageInfo(payload));
-    },
+    (payload) => dispatch(actionUpdatePicoPageInfo(payload)),
     [dispatch]
   );
   // Create a function that updates the store when the `pico.loaded` event is fired.
@@ -64,6 +58,25 @@ const Pico = (props) => {
 
   // Retrieve the Coral SSO token from the Redux store.
   const coralToken = useSelector(tokenSelector);
+
+  useEffect(() => {
+    const initHandler = () => {
+      setPicoInitialized(true);
+    };
+
+    // On component hydration, add an event listener to watch for the script's init event.
+    window.document.addEventListener('pico-init', initHandler);
+
+    return () => window.document.removeEventListener('pico-init', initHandler);
+  }, []);
+
+  useEffect(() => {
+    // Only update the store once Pico has loaded and if it has not already been
+    // updated in this component's lifecycle phase.
+    if (picoLoaded) {
+      dispatchUpdatePicoPageInfo(picoPageInfo);
+    }
+  }, [picoLoaded, picoPageInfo.url]);
 
   useEffect(() => {
     // See if the Pico widget container exists in the DOM.
@@ -80,28 +93,11 @@ const Pico = (props) => {
         ! picoLoaded
       ) {
         mountPicoNodes();
-        // Trigger the visit.
-        window.pico('visit', picoPageInfo);
         // Update the `pico` branch of the state tree and set `loaded` to true.
         dispatchPicoLoaded();
       }
     }
-
-    // Only update the store once Pico has loaded and if it has not already been
-    // updated in this component's lifecycle phase.
-    if (picoLoaded && ! picoPageInfoUpdated) {
-      dispatchUpdatePicoPageInfo(picoPageInfo);
-    }
-
-    const initHandler = () => {
-      setPicoInitialized(true);
-    };
-
-    // On component hydration, add an event listener to watch for the script's init event.
-    window.document.addEventListener('pico-init', initHandler);
-
-    return () => window.document.removeEventListener('pico-init', initHandler);
-  }, [picoInitialized, picoLoaded, picoPageInfoUpdated]);
+  }, [picoLoaded]);
 
   // Load the script for the first time.
   if (! picoInitialized) {

packages/integrations/sagas/picoSaga.js
@@ -50,10 +50,7 @@ function* dispatchPicoVisit() {
   const picoLoaded = yield select(picoLoadedSelector);
   const picoPageInfo = yield select(picoPageInfoSelector);
 
-  if (
-    (picoLoaded && picoPageInfo) &&
-    'function' === typeof window.Pico.handleNavigationEvent
-  ) {
+  if (picoLoaded && picoPageInfo) {
     window.pico('visit', picoPageInfo);
   }
 }

commit 86130c3560b97a45169f0d77e5878e3e7e68f507

fix(Pico): ensure Pico loads on first page render
This fixes a bug introduced in b590767 that caused Pico not to fire the initial page visit for the
initial pageview.

See: IRV-710

packages/integrations/components/pico/index.js
@@ -15,7 +15,10 @@ import {
 } from '../../actions/coralActions';
 import { tokenSelector } from '../../selectors/coralSelector';
 // Utility functions.
-import mountPicoNodes from './utils';
+import {
+  isPicoMounted,
+  mountPicoNodes,
+} from './utils';
 
 const Pico = (props) => {
   const {
@@ -83,21 +86,16 @@ const Pico = (props) => {
     const widgetContainer = document.getElementById('pico-widget-container');
 
     if (widgetContainer) {
-      // Prevent the widget script from being added more than once.
-      setPicoInitialized(true);
-
       // Ensure the target nodes are only mounted once on the initial server load.
-      if (
-        ! document.getElementById('PicoSignal-container') &&
-        ! document.getElementById('PicoRule-button') &&
-        ! picoLoaded
-      ) {
+      if (! isPicoMounted()) {
         mountPicoNodes();
+        // Dispatch initial Pico page visit.
+        dispatchUpdatePicoPageInfo(picoPageInfo);
         // Update the `pico` branch of the state tree and set `loaded` to true.
         dispatchPicoLoaded();
       }
     }
-  }, [picoLoaded]);
+  }, []);
 
   // Load the script for the first time.
   if (! picoInitialized) {

packages/integrations/components/pico/utils.js
@@ -47,13 +47,25 @@ function mountSignupFormNodes(documentBody) {
 }
 
 /**
+ * Helper function to see if Pico has already been mounted.
+ *
+ * @return {bool} Whether the Pico nodes are mounted in the DOM.
+ */
+export function isPicoMounted() {
+  return (
+    document.getElementById('PicoSignal-container') &&
+    document.getElementById('PicoRule-button')
+  );
+}
+
+/**
  * A function that mounts reference nodes into the DOM at initialization. These
  * nodes can be targeted by React components to trigger Pico actions if the component
  * requesting an action is rendered after Pico's initialization.
  *
  * @param {object} pageInfo The curren page info.
  */
-export default function mountPicoNodes() {
+export function mountPicoNodes() {
   // Get the `body` DOM node.
   const documentBody = document.getElementsByTagName('body')[0];
 