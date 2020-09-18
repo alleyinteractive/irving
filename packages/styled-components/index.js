f1d072870537bfd36399697ae18ae7172365e6a7
Owen Stowe <ostowe@gmail.com>
2020-09-18 11:45:20 -0400

    ci(travis): final test (hopefully)

--- packages/styled-components/index.js
+++ packages/styled-components/index.js
@@ -1,6 +1,7 @@
 import config from './irving.config';
 import createThemedComponentMap from './componentMap';
 
+// This is a comment about the themed component map.
 export { createThemedComponentMap };
 
 export default config;
