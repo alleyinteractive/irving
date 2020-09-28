03e8adb2fe2c307b01668cb0145ebe7930f5a568
Owen Stowe <ostowe@gmail.com>
2020-09-28 19:09:27 -0400

    fix(usepollfornode): rename polled node, move it

--- packages/integrations/components/pico/usePollForNode.js
+++ packages/integrations/components/pico/usePollForNode.js
@@ -1,17 +1,22 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * Set up an interval to poll for the presence of a specific DOM node.
+ *
+ * @param {string} selector A selector pointing to the node for which polling should be set up.
+ */
 const usePollForNode = (selector) => {
   const [node, setNode] = useState(null);
 
   useEffect(() => {
-    // See if a node exists in the DOM for provided selector.
-    const queriedNode = document.querySelector(selector);
-
     const pollForNode = setInterval(() => {
+      // See if a node exists in the DOM for provided selector.
+      const queriedNode = document.querySelector(selector);
+
       if (queriedNode) {
         // Prevent future polling events.
-        setNode(queriedNode);
         clearInterval(pollForNode);
+        setNode(queriedNode);
       }
     }, 250);
 
