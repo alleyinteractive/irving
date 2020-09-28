334c54c26bb0b3207f9af715036992a79942f743
James Burke <james.walter.burke@gmail.com>
2020-09-28 19:17:16 -0400

    feat(site theme recursion): allow the site theme to call itself and build a full tree

--- packages/styled/components/siteThemeProvider/index.js
+++ packages/styled/components/siteThemeProvider/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
+import get from 'lodash/get';
 
 /**
  * Component for using the `styled-components` package's ThemeProvider.
@@ -11,8 +12,37 @@ const SiteThemeProvider = (props) => {
     theme,
   } = props;
 
+  const recursivelyBuildTheme = (branch, tree) => {
+    const modifiedBranch = branch;
+
+    Object.keys(branch).forEach((key) => {
+      if ('object' === typeof branch[key]) {
+        recursivelyBuildTheme(branch[key], tree);
+      } else {
+        let returnValue = branch[key];
+        let defaultValue = returnValue;
+
+        // Recursively look for the returned value in the theme provider until
+        // the default is returned.
+        do {
+          defaultValue = returnValue;
+
+          returnValue = get(
+            tree,
+            returnValue,
+            defaultValue
+          );
+        } while (returnValue !== defaultValue);
+
+        modifiedBranch[key] = returnValue;
+      }
+    });
+
+    return modifiedBranch;
+  };
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={recursivelyBuildTheme(theme, theme)}>
       {children}
     </ThemeProvider>
   );
