ef03d94dbfed167cfacbea301f9bfeb751b846ab
James Burke <james.walter.burke@gmail.com>
2020-09-28 18:24:18 -0400

    feat(site themes): adds recursive lookup support to Site Themes

--- packages/styled/utils/siteTheme.js
+++ packages/styled/utils/siteTheme.js
@@ -1,7 +1,22 @@
 import get from 'lodash/get';
 
-const siteTheme = (valuePath, defaultValue = '') => (props) => (
-  get(props, `theme.${valuePath}`, defaultValue)
-);
+const siteTheme = (valuePath, defaultValue = '') => (props) => {
+  let returnValue = valuePath;
+  let loopDefaultValue = defaultValue;
+
+  // Recursively look for the returned value in the theme provider until
+  // the default is returned.
+  do {
+    loopDefaultValue = returnValue;
+
+    returnValue = get(
+      props,
+      `theme.${returnValue}`,
+      loopDefaultValue
+    );
+  } while (returnValue !== loopDefaultValue);
+
+  return returnValue;
+};
 
 export default siteTheme;
