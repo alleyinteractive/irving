19f6ebb96b3482bfeabd4e54cdad570411df2f34
Davis Shaver <davisshaver@gmail.com>
2020-10-09 14:53:07 -0400

    LEDE-329: Avoid unneeded ternary

--- packages/styled-components/components/image/index.js
+++ packages/styled-components/components/image/index.js
@@ -97,13 +97,13 @@ const Image = (props) => {
         <ImageTag
           alt={alt}
           aspectRatio={aspectRatio}
-          height={height ? height : undefined}
+          height={height || undefined}
           loading={loading}
           objectFit={objectFit}
           src={src || fallbackSrc}
           srcSet={srcset}
           sizes={getSizes()}
-          width={width ? width : undefined}
+          width={width || undefined}
         />
       </ImageWrapper>
       {(caption || credit) && showMeta && (
