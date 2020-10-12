e6e14f232deced092e19f36e498c293f1f7af9ed
Owen Stowe <ostowe@gmail.com>
2020-10-11 22:44:28 -0400

    fix(getapptemplatevars): fix some syntax

--- packages/loadable/server/getAppTemplateVars.js
+++ packages/loadable/server/getAppTemplateVars.js
@@ -1,23 +1,21 @@
 import React from 'react';
 
-/* eslint-disable */
+/* eslint-disable global-require, import/no-dynamic-require */
 export default function getAppTemplateVars(templateVars, clientStats) {
-  const { Wrapper } = templateVars;
-
-  /* eslint-disable global-require, import/no-dynamic-require */
   if (
     'development_server' === process.env.IRVING_EXECUTION_CONTEXT ||
     'production_server' === process.env.IRVING_EXECUTION_CONTEXT
   ) {
+    const { Wrapper } = templateVars;
     const { ChunkExtractor } = require('@loadable/server');
     const extractor = new ChunkExtractor({ stats: clientStats });
 
     return {
       Wrapper: () => extractor.collectChunks(<Wrapper />),
       head: {
-        script: [() => extractor.getScriptTags()],
-        link: [() => extractor.getLinkTags()],
-        style: [() => extractor.getStyleTags()],
+        script: () => extractor.getScriptTags(),
+        link: () => extractor.getLinkTags(),
+        style: () => extractor.getStyleTags(),
       },
     };
   }
