7c5dde0d439d6449a0dec4f2001a65fb8096378f
Owen Stowe <ostowe@gmail.com>
2020-10-13 17:17:36 -0400

    fix(loadable): add ssrtags config

--- packages/loadable/irving.config.js
+++ packages/loadable/irving.config.js
@@ -1,9 +1,11 @@
 import getAppTemplateVars from './server/getAppTemplateVars';
 import waitForClientRender from './client/waitForClientRender';
+import ssrTags from './client/ssrTags';
 
 // Export config
 export default {
   name: 'loadable',
   getAppTemplateVars,
   waitForClientRender,
+  ssrTags,
 };
