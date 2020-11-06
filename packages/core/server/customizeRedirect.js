b18422b47fe04016bbe70d47147e04b18e958d4b
Tyler Reckart <tyler@alley.co>
2020-11-06 13:24:18 -0500

    IRV-723: custom redirect proof of concept

--- packages/core/server/customizeRedirect.js
+++ packages/core/server/customizeRedirect.js
@@ -1,31 +1,55 @@
-const expressNakedRedirect = require('express-naked-redirect');
-const getValueFromFiles = require('../config/irving/getValueFromFiles');
+// const getValueFromFiles = require('../config/irving/getValueFromFiles');
 const {
   NODE_ENV,
   ROOT_URL,
 } = process.env;
-const config = getValueFromFiles(
-  'server/customizeRedirect.js',
-  {}
-);
+
+// TODO: configure irving.config.js to serve this configuration object that
+// sets the root host to redirect to and the protocol to enforce.
+//
+// const { redirectHost: config } = getValueFromFiles(
+//   'irving.config.js',
+//   {}
+// );
+//
+// If the host does not exist, the function should exit per the customizeRedirect
+// function below.
+const config = {
+  host: 'irving.alley.test',
+  protocol: 'https',
+};
 
 const customizeRedirect = () => (
   (req, res, next) => {
+    const hostMatch = ROOT_URL.includes(req.headers.host);
+    const hasConfig = 0 !== Object.keys(config).length;
+    const isDev = 'development' === NODE_ENV;
+
     /**
      * Move on if:
-     * - no config was added
      * - current request is not for the configured ROOT_URL
+     * - no config was added
      * - in a dev environment
      */
-    if (
-      ! ROOT_URL.includes(req.headers.host) ||
-      0 === Object.keys(config).length ||
-      'development' === NODE_ENV
-    ) {
+    if (! hostMatch || ! hasConfig || isDev) {
       return next();
     }
 
-    return expressNakedRedirect(config)(req, res, next);
+    const requestProtocol = req.headers['x-forwarded-proto'] || req.protocol;
+
+    const {
+      host,
+      protocol,
+    } = config;
+
+    if (
+      protocol !== requestProtocol ||
+      host !== req.get('Host')
+    ) {
+      return res.redirect([`${protocol}://${host}`, req.url].join(''));
+    }
+
+    return res.redirect([`${requestProtocol}://${host}`, req.url].join(''));
   }
 );
 
