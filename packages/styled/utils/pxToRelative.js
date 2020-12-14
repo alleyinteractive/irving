e75702fe5fb4378ccd9eb75c4810b9426e8ef9a0
Owen Stowe <ostowe@gmail.com>
2020-12-14 14:59:00 -0500

    feat(sitetheme): changes to make styled utils less verbose, including em rem and siteTheme

--- packages/styled/utils/pxToRelative.js
+++ packages/styled/utils/pxToRelative.js
@@ -1,6 +1,33 @@
-// Convert a pixel value to a relative one using a base font size.
-const pxToRelative = (value, base, unit) => (
-  `${parseFloat(value) / base}${unit}`
+/**
+ * Create a function for converting px values to the relative unit of your choice.
+ *
+ * @param {string} unit Relative unit to which px value should be converted.
+ * @param {integer} base Base font size to use as conversion ratio.
+ * @return {function} Converter function
+ */
+export const createRelativeConverter = (unit, base = 16) => (
+  /**
+   * Convert array of px values to relative unit defined in the outer scope.
+   *
+   * @param {number} pxVal Value(s) to convert.
+   * @return {function} Props function for use in styled components.
+   */
+  (...pxVal) => (
+    (props) => {
+      const convertedValues = pxVal.map(
+        (value) => {
+          const normalizedValue = 'function' === typeof value ?
+            value(props) : value;
+
+          return `${parseFloat(normalizedValue) / base}${unit}`;
+        }
+      );
+
+      return convertedValues.join(' ');
+    }
+  )
 );
 
-export default pxToRelative;
+// Default function using 16px base.
+export const rem = createRelativeConverter('rem');
+export const em = createRelativeConverter('em');
