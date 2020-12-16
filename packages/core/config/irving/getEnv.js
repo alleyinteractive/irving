10e023a9f83da83022b99f45e879cd745b0593bf
Owen Stowe <ostowe@gmail.com>
2020-12-15 19:58:14 -0500

    fix(env): remove env logging

--- packages/core/config/irving/getEnv.js
+++ packages/core/config/irving/getEnv.js
@@ -1,30 +1,22 @@
 const uniq = require('lodash/uniq');
-let logService;
 let multisiteConfig;
 
 /* eslint-disable global-require, import/no-dynamic-require */
 if (process.env.IRVING_EXECUTION_CONTEXT) {
   multisiteConfig = require('@irvingjs/multisite.config');
-  logService = require('@irvingjs/services/logService');
 } else {
   const { maybeResolveBuildModule } = require('../../utils/userModule');
-  logService = require(
-    '../../services/logService/getServiceFromFilesystem'
-  );
   multisiteConfig = require(
     maybeResolveBuildModule('multisite.config.js')
   );
 }
 /* eslint-enable */
 
-// Create logger
-const log = logService('irving:env');
-
 // If a configuration exists, ensure no site config appears twice.
 const config = multisiteConfig.length ?
   uniq(multisiteConfig, 'domain').flat() :
   [];
-
+cd
 /**
  * A utility function that replaces the `ROOT_URL` and `API_ROOT_URL` values
  * in the process.env object.
@@ -38,13 +30,6 @@ module.exports = (hostname = window.location.hostname) => {
   );
 
   if (- 1 === hostIndex) {
-    log.info(
-      'using default env: %o',
-      {
-        API_ROOT_URL: env.API_ROOT_URL,
-        ROOT_URL: env.ROOT_URL,
-      }
-    );
     return env;
   }
 
@@ -52,14 +37,6 @@ module.exports = (hostname = window.location.hostname) => {
     ...env,
     ...config[hostIndex].vars,
   };
-
-  log.info(
-    `using env for site ${hostname}: %o`,
-    {
-      API_ROOT_URL: multisiteEnv.API_ROOT_URL,
-      ROOT_URL: multisiteEnv.ROOT_URL,
-    }
-  );
 
   return multisiteEnv;
 };
