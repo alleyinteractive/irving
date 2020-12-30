cb8145efe42bf97c6552a9e06716c7a069ffdce2
Davis Shaver <davis@alley.co>
2020-12-30 16:06:36 -0500

    LEDE-678: Improve type safety for payload variable

--- packages/integrations/sagas/picoSaga.js
+++ packages/integrations/sagas/picoSaga.js
@@ -108,6 +108,6 @@ function trackUpdatePicoSignal(signal) {
   window.dataLayer = window.dataLayer ?? [];
   window.dataLayer.push({
     event: 'irving.updatePicoSignal',
-    picoSignal: signal.payload,
+    picoSignal: signal.payload ?? {},
   });
 }
