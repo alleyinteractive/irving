5bfd029d723d456e49dfc4e6706139dba3cff530
Renato Alves <19148962+renatonascalves@users.noreply.github.com>
2021-04-06 15:52:41 -0300

    Button: more examples and storybook documentation

--- packages/styled-components/components/button/button.stories.js
+++ packages/styled-components/components/button/button.stories.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Button from '.';
+import Text from '../text';
 
 export default {
   component: Button,
   title: 'Styled Components|Button',
 };
 
+// Basic Button Example.
 export const BasicExample = () => <Button>Button Example</Button>;
+
+// Basic Button with onClick Handler.
+const clickHandler = () => {};
+export const OnClickExample = () => <Button onClick={clickHandler}>onClick Button Example</Button>;
+
+// Button example with children.
+export const ButtonChildren = () => <Button><Text content="Cool Button" /></Button>;
