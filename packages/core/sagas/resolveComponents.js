45a6338fcff1b12c8501ff20f9fbbb0ddf6d5e20
Owen Stowe <ostowe@gmail.com>
2021-04-23 11:38:45 -0400

    fix(redirect): return early from components saga if we aren't redirecting

--- packages/core/sagas/resolveComponents.js
+++ packages/core/sagas/resolveComponents.js
@@ -58,10 +58,11 @@ export default function* resolveComponents() {
       context
     );
 
-    // Don't receive components on client side if redirecting,
-    // otherwise will result in a confusing flash of empty page content.
+    // Only redirect on client side and if there's, otherwise just receive components
+    // otherwise there's a strange flash of content.
     if ((result.redirectTo && isNode()) || ! result.redirectTo) {
       yield put(actionReceiveComponents(result));
+      return;
     }
 
     // Request needs to be redirected.
