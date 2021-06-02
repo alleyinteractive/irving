da98243b81edfb7b9be02ddc6f33df2a0727469a
Owen Stowe <ostowe@gmail.com>
2021-06-02 14:04:31 -0400

    fix(test): remove test log that made it into main branch

--- packages/core/services/fetchComponents.js
+++ packages/core/services/fetchComponents.js
@@ -22,15 +22,9 @@ export async function fetchComponents(routeMeta, routeCookies) {
     cookie,
     hostname,
   } = routeMeta;
-  const { FETCH_TIMEOUT } = getEnv(hostname);
+  const env = getEnv(hostname);
+  const { FETCH_TIMEOUT } = env;
   const apiUrl = createEndpointUrl(routeMeta, routeCookies);
-
-  log.error(
-    'OWEN TEST',
-    {
-      tags: createRouteLogTags(routeMeta, env),
-    }
-  );
 
   // Create abort controller and set timeout to abort fetch call.
   // Default timeout is 10s, but can be configured with env var.
