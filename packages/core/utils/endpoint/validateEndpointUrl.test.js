5ccbf9ad3b034cdfb943e2bab42157b7d90b63aa
justinwhall <justin@alley.co>
2021-06-17 12:58:11 -0600

    wip

--- packages/core/utils/endpoint/validateEndpointUrl.test.js
+++ packages/core/utils/endpoint/validateEndpointUrl.test.js
@@ -1,6 +1,7 @@
 import validateEndpointUrl from './validateEndpointUrl';
 
 describe('validateEndpointUrl', () => {
+    const hostname = 'multisite-one.irving.test';
     it('Should return false', () => {
         const endpoint = validateEndpointUrl();
         expect(endpoint).toBe(false);
