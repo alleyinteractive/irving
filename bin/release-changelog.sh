f91d26574690ccdb9a72d72bf7cba2a8de55e4a7
Owen Stowe <ostowe@gmail.com>
2021-06-30 19:13:41 -0400

    build(release): remove old release-changelog.sh script

--- bin/release-changelog.sh
+++
@@ -1,4 +0,0 @@
-ag -o --no-numbers --no-filename "(#\s\[$1[.\d\-]+((alpha|beta|rc)[.\d]+)?\]\([^)]*\)\s\([0-9\-]+\))(?![\r\n]+\*\*Note:\*\*)[\s\S]+?(?=\n#\s)" CHANGELOG.md |
-sed '/^$/d' |
-sed 's/### /#### /' |
-sed 's/# \[/### \[/'
