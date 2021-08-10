783d1a0c3fbf648015caaf0b0116e23a2f51c87f
justinwhall <justin@alley.co>
2021-08-10 10:13:36 -0600

    feat(logging): update logService to support debug package

--- packages/vip-go/services/logService.js
+++ packages/vip-go/services/logService.js
@@ -4,7 +4,10 @@ const defaultService = require(
 const getEnv = require('@irvingjs/core/utils/universalEnv');
 const monitor = require('@irvingjs/core/services/monitorService/getService')();
 const SentryReact = require('@sentry/react');
+const debug = require('debug');
 const generateLogInfo = require('./generateLogInfo');
+
+let initialized = false;
 
 /**
  * Create a debug logger that will conditionally handle logged errors based on
@@ -19,8 +22,10 @@ const getService = (namespace) => {
   let service = defaultService;
   let Sentry;
 
+  // Init logger with proper namespace.
+  const logger = debug(namespace);
+
   const {
-    ROOT_URL,
     NODE_ENV,
     IRVING_APP_ENVIRONMENT,
     IRVING_RENDER_ERRORS,
@@ -28,50 +33,56 @@ const getService = (namespace) => {
     SENTRY_ENVIRONMENT,
   } = getEnv();
 
-  // /* eslint-disable global-require */
+  // Determine if we should log to Sentry.
+  const logToSentry = (SENTRY_DSN && NODE_ENV === 'production')
+    || (SENTRY_DSN && IRVING_RENDER_ERRORS);
+
   if (
     !process.env.IRVING_EXECUTION_CONTEXT
     || process.env.IRVING_EXECUTION_CONTEXT === 'production_server'
     || process.env.IRVING_EXECUTION_CONTEXT === 'development_server'
   ) {
+    // eslint-disable-next-line global-require
     Sentry = require('@sentry/node');
   } else {
     Sentry = SentryReact;
   }
 
-  Sentry.init({
-    dsn: 'https://a51b194a39f84d26848eff7cc169f089@o576571.ingest.sentry.io/5730320',
-    environment: 'JUSTIN_IRVING_LOCAL',
-    // integrations: [new Integrations.BrowserTracing()],
-
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    // tracesSampleRate: 1.0,
-  });
+  if (logToSentry && !initialized) {
+    Sentry.init({
+      dsn: SENTRY_DSN,
+      environment: SENTRY_ENVIRONMENT || IRVING_APP_ENVIRONMENT || NODE_ENV,
+    });
+    initialized = true;
+  }
 
   // Map log levels to winston log levels in node.
   service = Object.keys(defaultService)
     .reduce((acc, method) => {
       acc[method] = (...messages) => {
-        // Construct log info object and send it to winston.
+        // Construct log info object & log it with debug.
         const logInfo = generateLogInfo(method, messages);
-        // console.log('>>>>>>>>');
-        // console.log(logInfo);
-        // log[method](logInfo);
+        logger(...logInfo.message);
 
         // If we have an error, do a couple more things with it.
         if (logInfo.level === 'error') {
           const err = new Error(logInfo.message);
 
-          Sentry.withScope((scope) => {
-            scope.setLevel('error');
-            Sentry.captureException(err);
-          });
+          if (logToSentry) {
+            Sentry.withScope((scope) => {
+              scope.setLevel('error');
+              Sentry.captureException(err, {
+                contexts: logInfo.contexts,
+                extra: logInfo.extra,
+                tags: logInfo.tags,
+              });
+            });
+          }
 
-          // if (NODE_ENV === 'development' && !IRVING_RENDER_ERRORS) {
-          //   // In development the app should crash fast when encountering any errors.
-          //   throw err;
-          // }
+          if (NODE_ENV === 'development' && !IRVING_RENDER_ERRORS) {
+            // In development the app should crash fast when encountering any errors.
+            throw err;
+          }
 
           // Send error to production monitoring service.
           if (NODE_ENV === 'production') {
@@ -81,7 +92,6 @@ const getService = (namespace) => {
       };
       return acc;
     }, {});
-  /* eslint-enable */
 
   return service;
 };
