ebf6d2fc9df537e6d3425ddaa81073d4edac5f2e
justinwhall <justin@alley.co>
2021-08-20 12:14:45 -0600

    update test

--- packages/core/config/irving/getServiceAliases.test.js
+++ packages/core/config/irving/getServiceAliases.test.js
@@ -25,10 +25,6 @@ describe('getServiceAliases', () => {
         process.cwd(),
         'node_modules/@irvingjs/test-package-two/services/logService.js'
       ),
-      '@irvingjs/services/logServiceConfig': path.join(
-        process.cwd(),
-        '/services/logService/sentry.config.js'
-      ),
       '@irvingjs/services/monitorService': path.join(
         process.cwd(),
         'services/monitorService/defaultService.js'
@@ -55,10 +51,6 @@ describe('getServiceAliases', () => {
       '@irvingjs/services/logService': path.join(
         process.cwd(),
         'node_modules/@irvingjs/test-package-two/services/logService.js'
-      ),
-      '@irvingjs/services/logServiceConfig': path.join(
-        process.cwd(),
-        '/services/logService/sentry.config.js'
       ),
       '@irvingjs/services/monitorService': path.join(
         process.cwd(),
