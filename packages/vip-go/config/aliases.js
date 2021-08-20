3ae65347787a64f3bd6756c54c5cc3ba6736a425
justinwhall <justin@alley.co>
2021-08-20 15:03:51 -0600

    refactor(logging): refactor sentry config to match client/server

--- packages/vip-go/config/aliases.js
+++ packages/vip-go/config/aliases.js
@@ -1,17 +1,5 @@
-const path = require('path');
-const resolveConfigFilepath = require('@irvingjs/core/config/irving/resolveConfigFilepath');
-
-/**
- * Get the Sentry config.
- */
-let sentryConfig = resolveConfigFilepath('sentry.config.js');
-if (!sentryConfig) {
-  sentryConfig = path.join(
-    __dirname,
-    'sentry.config.js',
-  );
-}
+const getSentryConfig = require('./getSentryConfig');
 
 module.exports = {
-  '@irvingjs/config/sentryConfig': sentryConfig,
+  '@irvingjs/config/sentryConfig': getSentryConfig(),
 };
